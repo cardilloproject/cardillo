0f891b216b09a3c913bb251c90242a421e5931c2
eugstesi <30239455+eugstesi@users.noreply.github.com>
2020-08-06 19:15:23 +0200

    linear guidance 3D without stiffness

--- cardillo/model/bilateral_constraints/implicit/__init__.py
+++ cardillo/model/bilateral_constraints/implicit/__init__.py
@@ -2,3 +2,4 @@ from .rod import Rod
 from .spherical_joint import Spherical_joint, Spherical_joint2D
 from .rigid_connection import Rigid_connection, Rigid_connection2D
 from .revolute_joint import Revolute_joint
+from .linear_guidance import Linear_guidance
--- cardillo/model/bilateral_constraints/implicit/linear_guidance.py
+++ cardillo/model/bilateral_constraints/implicit/linear_guidance.py
@@ -26,7 +26,7 @@ class Linear_guidance():
         self.uDOF = np.concatenate([self.subsystem1.uDOF[uDOF1], self.subsystem2.uDOF[uDOF2]])
         self.nu1 = len(uDOF1)
         self.nu2 = len(uDOF2)
-        self.__nu = self.nu1 + self.nu2
+        self._nu = self.nu1 + self.nu2
         
         A_IK1 = self.subsystem1.A_IK(self.subsystem1.t0, self.subsystem1.q0[qDOF1], self.frame_ID1)
         A_IK2 = self.subsystem2.A_IK(self.subsystem2.t0, self.subsystem2.q0[qDOF2], self.frame_ID2)
@@ -61,15 +61,14 @@ class Linear_guidance():
         self.J_R2_q = lambda t, q: np.einsum('ijk,jl->ilk', self.subsystem2.A_IK_q(t, q[nq1:], self.frame_ID2), self.subsystem2.K_J_R(t, q[nq1:], self.frame_ID2) ) + np.einsum('ij,jkl->ikl', self.subsystem2.A_IK(t, q[nq1:], self.frame_ID2), self.subsystem2.K_J_R_q(t, q[nq1:], self.frame_ID2) )
 
         q0 = np.concatenate([self.subsystem1.q0[qDOF1], self.subsystem2.q0[qDOF2]])
-        self.angle0 = self.angle(self.t0, q0)
 
     def g(self, t, q):
         r_OP1 = self.r_OP1(t, q) 
         r_OP2 = self.r_OP2(t, q)
         r_P1P2 = r_OP2 - r_OP1
-        ex1, ey1, ez1 = self.A_IB1(t, q).T
+        ex1, _, _ = self.A_IB1(t, q).T
         _, ey2, ez2 = self.A_IB2(t, q).T
-        return np.array([r_P1P2 @ ey1, r_P1P2 @ ez1, ex1 @ ey2, ex1 @ ez2])
+        return np.array([r_P1P2 @ ey2, r_P1P2 @ ez2, ex1 @ ey2, ex1 @ ez2])
 
     def g_q_dense(self, t, q):
         nq1 = self.nq1
@@ -78,25 +77,24 @@ class Linear_guidance():
         r_OP1 = self.r_OP1(t, q) 
         r_OP2 = self.r_OP2(t, q)
         r_P1P2 = r_OP2 - r_OP1
-        ex1, ey1, ez1 = self.A_IB1(t, q).T
+        ex1, _, _ = self.A_IB1(t, q).T
         _, ey2, ez2 = self.A_IB2(t, q).T
 
-        g_q[:3, :nq1] = - self.r_OP1_q(t, q) 
-        g_q[:3, nq1:] = self.r_OP2_q(t, q)
-
-        ez1 = self.A_IB1(t, q)[:, 2]
-        ex2, ey2 = self.A_IB2(t, q)[:, :2].T
-
         A_IB1_q = self.A_IB1_q(t, q)
-        ez1_q = A_IB1_q[:, 2]
+        ex1_q = A_IB1_q[:, 0]
         A_IB2_q = self.A_IB2_q(t, q)
-        ex2_q = A_IB2_q[:, 0]
         ey2_q = A_IB2_q[:, 1]
-
-        g_q[3, :nq1] = ex2 @ ez1_q
-        g_q[3, nq1:] = ez1 @ ex2_q
-        g_q[4, :nq1] = ey2 @ ez1_q
-        g_q[4, nq1:] = ez1 @ ey2_q
+        ez2_q = A_IB2_q[:, 2]
+        
+        # np.array([r_P1P2 @ ey2, r_P1P2 @ ez2, ex1 @ ey2, ex1 @ ez2])
+        g_q[0, :nq1] = -ey2 @ self.r_OP1_q(t, q) 
+        g_q[0, nq1:] =  ey2 @ self.r_OP2_q(t, q) + r_OP2 @ ey2_q
+        g_q[1, :nq1] = -ez2 @ self.r_OP1_q(t, q) 
+        g_q[1, nq1:] =  ez2 @ self.r_OP2_q(t, q) + r_OP2 @ ez2_q
+        g_q[2, :nq1] = ey2 @ ex1_q
+        g_q[2, nq1:] = ex1 @ ey2_q
+        g_q[3, :nq1] = ez2 @ ex1_q
+        g_q[3, nq1:] = ex1 @ ez2_q
         return g_q
 
     def g_q(self, t, q, coo):
@@ -104,22 +102,28 @@ class Linear_guidance():
    
     def W_g_dense(self, t, q):
         nu1 = self.nu1
-        W_g = np.zeros((self.__nu, self.nla_g))
+        W_g = np.zeros((self._nu, self.nla_g))
 
+        r_OP1 = self.r_OP1(t, q) 
+        r_OP2 = self.r_OP2(t, q)
+        r_P1P2 = r_OP2 - r_OP1
+        ex1, _, _ = self.A_IB1(t, q).T
+        _, ey2, ez2 = self.A_IB2(t, q).T
+
+        # np.array([r_P1P2 @ ey2, r_P1P2 @ ez2, ex1 @ ey2, ex1 @ ez2])
         # position 
         J_P1 = self.J_P1(t, q) 
         J_P2 = self.J_P2(t, q)
-        W_g[:nu1, :3] = -J_P1.T
-        W_g[nu1:, :3] = J_P2.T
+        J_R1 = self.J_R1(t, q)
+        J_R2 = self.J_R2(t, q)
+        J = np.hstack([J_R1, -J_R2])
 
-        # angular velocity
-        ez1 = self.A_IB1(t, q)[:, 2]
-        ex2, ey2 = self.A_IB2(t, q)[:, :2].T
-        
-        J = np.hstack([self.J_R1(t, q), -self.J_R2(t, q)])
-
-        W_g[:, 3] = cross3(ez1, ex2) @ J
-        W_g[:, 4] = cross3(ez1, ey2) @ J
+        W_g[:nu1, 0] = -ey2 @ J_P1
+        W_g[nu1:, 0] =  ey2 @ J_P2 + cross3(ey2, r_P1P2) @ J_R2
+        W_g[:nu1, 1] = -ez2 @ J_P1
+        W_g[nu1:, 1] =  ez2 @ J_P2 + cross3(ez2, r_P1P2) @ J_R2
+        W_g[:, 2] = cross3(ex1, ey2) @ J
+        W_g[:, 3] = cross3(ex1, ez2) @ J
 
         return W_g
 
@@ -127,56 +131,57 @@ class Linear_guidance():
         coo.extend(self.W_g_dense(t, q), (self.uDOF, self.la_gDOF))
 
     def Wla_g_q(self, t, q, la_g, coo):
-        nq1 = self.nq1
-        nu1 = self.nu1
-        dense = np.zeros((self.__nu, self.__nq))
+        pass
+        # nq1 = self.nq1
+        # nu1 = self.nu1
+        # dense = np.zeros((self._nu, self.__nq))
 
-        # position 
-        J_P1_q = self.J_P1_q(t, q) 
-        J_P2_q = self.J_P2_q(t, q)
-        dense[:nu1, :nq1] += np.einsum('i,ijk->jk', -la_g[:3], J_P1_q)
-        dense[nu1:, nq1:] += np.einsum('i,ijk->jk', la_g[:3], J_P2_q)
+        # # position 
+        # J_P1_q = self.J_P1_q(t, q) 
+        # J_P2_q = self.J_P2_q(t, q)
+        # dense[:nu1, :nq1] += np.einsum('i,ijk->jk', -la_g[:3], J_P1_q)
+        # dense[nu1:, nq1:] += np.einsum('i,ijk->jk', la_g[:3], J_P2_q)
 
-        # angular velocity
-        ez1 = self.A_IB1(t, q)[:, 2]
-        ex2, ey2 = self.A_IB2(t, q)[:, :2].T
+        # # angular velocity
+        # ez1 = self.A_IB1(t, q)[:, 2]
+        # ex2, ey2 = self.A_IB2(t, q)[:, :2].T
         
-        A_IB1_q = self.A_IB1_q(t, q)
-        ez1_q = A_IB1_q[:, 2]
-        A_IB2_q = self.A_IB2_q(t, q)
-        ex2_q = A_IB2_q[:, 0]
-        ey2_q = A_IB2_q[:, 1]
+        # A_IB1_q = self.A_IB1_q(t, q)
+        # ez1_q = A_IB1_q[:, 2]
+        # A_IB2_q = self.A_IB2_q(t, q)
+        # ex2_q = A_IB2_q[:, 0]
+        # ey2_q = A_IB2_q[:, 1]
 
-        J_R1 = self.J_R1(t, q)
-        J_R2 = self.J_R2(t, q)
-        J_R1_q = self.J_R1_q(t, q)
-        J_R2_q = self.J_R2_q(t, q)
+        # J_R1 = self.J_R1(t, q)
+        # J_R2 = self.J_R2(t, q)
+        # J_R1_q = self.J_R1_q(t, q)
+        # J_R2_q = self.J_R2_q(t, q)
 
-        # W_g[:nu1, 3] la_g[3]= cross3(ez1, ex2) @ J_R1 * la_g[3]
-        # W_g[nu1:, 3] la_g[3]= - cross3(ez1, ex2) @ J_R2 * la_g[3]
-        n = cross3(ez1, ex2)
-        n_q1 = -ax2skew(ex2) @ ez1_q
-        n_q2 = ax2skew(ez1) @ ex2_q
-        dense[:nu1, :nq1] += np.einsum('i,ijk->jk', la_g[3] * n, J_R1_q) \
-                                + np.einsum('ij,ik->kj', la_g[3] * n_q1, J_R1)
-        dense[:nu1, nq1:] += np.einsum('ij,ik->kj', la_g[3] * n_q2, J_R1)
-        dense[nu1:, :nq1] += np.einsum('ij,ik->kj', - la_g[3] * n_q1, J_R2)
-        dense[nu1:, nq1:] += np.einsum('i,ijk->jk', - la_g[3] * n, J_R2_q) \
-                                + np.einsum('ij,ik->kj', - la_g[3] * n_q2, J_R2)
+        # # W_g[:nu1, 3] la_g[3]= cross3(ez1, ex2) @ J_R1 * la_g[3]
+        # # W_g[nu1:, 3] la_g[3]= - cross3(ez1, ex2) @ J_R2 * la_g[3]
+        # n = cross3(ez1, ex2)
+        # n_q1 = -ax2skew(ex2) @ ez1_q
+        # n_q2 = ax2skew(ez1) @ ex2_q
+        # dense[:nu1, :nq1] += np.einsum('i,ijk->jk', la_g[3] * n, J_R1_q) \
+        #                         + np.einsum('ij,ik->kj', la_g[3] * n_q1, J_R1)
+        # dense[:nu1, nq1:] += np.einsum('ij,ik->kj', la_g[3] * n_q2, J_R1)
+        # dense[nu1:, :nq1] += np.einsum('ij,ik->kj', - la_g[3] * n_q1, J_R2)
+        # dense[nu1:, nq1:] += np.einsum('i,ijk->jk', - la_g[3] * n, J_R2_q) \
+        #                         + np.einsum('ij,ik->kj', - la_g[3] * n_q2, J_R2)
         
-        # W_g[:nu1, 4] la_g[4]= cross3(ez1, ey2) @ J_R1 * la_g[4]
-        # W_g[nu1:, 4] la_g[4]= - cross3(ez1, ey2) @ J_R2 * la_g[4]
-        n = cross3(ez1, ey2)
-        n_q1 = -ax2skew(ey2) @ ez1_q
-        n_q2 = ax2skew(ez1) @ ey2_q
-        dense[:nu1, :nq1] += np.einsum('i,ijk->jk', la_g[4] * n, J_R1_q) \
-                                + np.einsum('ij,ik->kj', la_g[4] * n_q1, J_R1)
-        dense[:nu1, nq1:] += np.einsum('ij,ik->kj', la_g[4] * n_q2, J_R1)
-        dense[nu1:, :nq1] += np.einsum('ij,ik->kj', - la_g[4] * n_q1, J_R2)
-        dense[nu1:, nq1:] += np.einsum('i,ijk->jk', - la_g[4] * n, J_R2_q) \
-                                + np.einsum('ij,ik->kj', - la_g[4] * n_q2, J_R2)
+        # # W_g[:nu1, 4] la_g[4]= cross3(ez1, ey2) @ J_R1 * la_g[4]
+        # # W_g[nu1:, 4] la_g[4]= - cross3(ez1, ey2) @ J_R2 * la_g[4]
+        # n = cross3(ez1, ey2)
+        # n_q1 = -ax2skew(ey2) @ ez1_q
+        # n_q2 = ax2skew(ez1) @ ey2_q
+        # dense[:nu1, :nq1] += np.einsum('i,ijk->jk', la_g[4] * n, J_R1_q) \
+        #                         + np.einsum('ij,ik->kj', la_g[4] * n_q1, J_R1)
+        # dense[:nu1, nq1:] += np.einsum('ij,ik->kj', la_g[4] * n_q2, J_R1)
+        # dense[nu1:, :nq1] += np.einsum('ij,ik->kj', - la_g[4] * n_q1, J_R2)
+        # dense[nu1:, nq1:] += np.einsum('i,ijk->jk', - la_g[4] * n, J_R2_q) \
+        #                         + np.einsum('ij,ik->kj', - la_g[4] * n_q2, J_R2)
 
-        coo.extend( dense, (self.uDOF, self.qDOF))
+        # coo.extend( dense, (self.uDOF, self.qDOF))
 
 # class Linear_guidance2D(Linear_guidance):
 #     def __init__(self, *args, **kwargs):
