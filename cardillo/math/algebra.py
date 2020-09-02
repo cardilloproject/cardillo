jacobian for generalized alpha4; cleanup algebra module; corrected angular velocity; J_R and J_R_q in director beam

cardillo/math/algebra.py
@@ -16,11 +16,20 @@ def norm4(a):
     return sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2] + a[3] * a[3])
 
 def skew2ax(A):
-    # build skew symmetric matrix first and then compute axial vector
-    B = 0.5 * (A - A.T)
-    return np.array([B[2, 1], B[0, 2], B[1, 0]])
+    return 0.5 * np.array([A[2, 1] - A[1, 2], A[0, 2] - A[2, 0], A[1, 0] - A[0, 1]])
 
-# see https://pythonpath.wordpress.com/2012/09/04/skew-with-numpy-operations/
+def skew2ax_A():
+    A = np.zeros((3, 3, 3))
+    A[0, 2, 1] = 0.5
+    A[0, 1, 2] = -0.5
+
+    A[1, 0, 2] = 0.5
+    A[1, 2, 0] = -0.5
+
+    A[2, 1, 0] = 0.5
+    A[2, 0, 1] = -0.5
+    return A
+
 def ax2skew(a):
     return np.array([[0,    -a[2], a[1] ],
                      [a[2],  0,    -a[0]],
@@ -36,29 +45,10 @@ def ax2skew_a():
     A[1, 0, 2] =  1
     return A
 
-def ax2skew2(a):
-    skv = np.roll(np.roll(np.diag(a.flatten()), 1, 1), -1, 0)
-    return skv - skv.T
-
 def cross3(a, b):
     return np.array([a[1] * b[2] - a[2] * b[1], \
                      a[2] * b[0] - a[0] * b[2], \
                      a[0] * b[1] - a[1] * b[0] ])
-
-# TODO: think of a test for this function
-def cross_nm(a, b):
-    na0 = a.shape[0]
-    na1 = a.shape[1]
-    nb0 = b.shape[0]
-    nb1 = b.shape[1]
-    A = np.zeros((na0, na1, nb1))
-    for i in range(na0):
-        for n in range(na1):
-            for m in range(nb1):
-                for j in range(na0):
-                    for k in range(nb0):
-                        A[i, n, m] += LeviCivita3(i, j, k) * a[k, n] * b[j, m]
-    return A
 
 def LeviCivita3(i, j, k):
     return (i - j) * (j - k) * (k - i) / 2

cardillo/model/bilateral_constraints/implicit/rigid_connection.py
@@ -96,6 +96,14 @@ class Rigid_connection():
         g_q[5, nq1:] = ez1 @ ex2_q
         return g_q
 
+        # g_q_num = Numerical_derivative(self.g, order=2)._x(t, q)
+        # diff = g_q_num - g_q
+        # diff_error = diff
+        # error = np.linalg.norm(diff_error)
+        # print(f'error g_q: {error}')
+        # return g_q_num
+
+    # TODO:
     def g_dot(self, t, q, u):
         g_dot = np.zeros(self.nla_g)
 
@@ -110,6 +118,20 @@ class Rigid_connection():
         g_dot[4] = cross3(ey1, ez2) @ Omega21
         g_dot[5] = cross3(ez1, ex2) @ Omega21
         return g_dot
+
+        # g_dot_num = Numerical_derivative(self.g, order=2)._t(t, q)
+        # # g_dot_num += self.g_q_dense(t, q) @ u
+        # g_dot_num += Numerical_derivative(self.g, order=2)._x(t, q) @ u
+        # # diff = g_dot_num - g_dot
+        # # diff_error = diff[3:]
+        # # error = np.linalg.norm(diff_error)
+        # # print(f'error g_dot: {error}')
+        # return g_dot_num
+
+    # TODO:
+    def g_dot_q(self, t, q, u, coo):
+        dense = Numerical_derivative(self.g_dot, order=2)._x(t, q, u)
+        coo.extend(dense, (self.la_gDOF, self.qDOF))
 
     def g_dot_u(self, t, q, coo):
         coo.extend(self.W_g_dense(t, q).T, (self.la_gDOF, self.uDOF))
@@ -145,6 +167,24 @@ class Rigid_connection():
 
         return g_ddot
 
+        # g_ddot_num = Numerical_derivative(self.g_dot)._t(t, q, u)
+        # g_ddot_num += Numerical_derivative(self.g_dot)._x(t, q, u) @ u
+        # g_ddot_num += Numerical_derivative(self.g_dot)._y(t, q, u) @ u_dot
+        # diff = g_ddot_num - g_ddot
+        # error = np.linalg.norm(diff)
+        # print(f'error g_ddot: {error}')
+        # return g_ddot_num
+
+    # TODO:
+    def g_ddot_q(self, t, q, u, u_dot, coo):
+        dense = Numerical_derivative(lambda t, q, u: self.g_ddot(t, q, u, u_dot), order=2)._x(t, q, u)
+        coo.extend(dense, (self.la_gDOF, self.qDOF))
+
+    # TODO:
+    def g_ddot_u(self, t, q, u, u_dot, coo):
+        dense = Numerical_derivative(lambda t, q, u: self.g_ddot(t, q, u, u_dot), order=2)._y(t, q, u)
+        coo.extend(dense, (self.la_gDOF, self.uDOF))
+
     def g_q(self, t, q, coo):
         coo.extend(self.g_q_dense(t, q), (self.la_gDOF, self.qDOF))
    
@@ -158,16 +198,22 @@ class Rigid_connection():
         W_g[:nu1, :3] = -J_P1.T
         W_g[nu1:, :3] = J_P2.T
 
-        # angular velocity
+        # orientations
         ex1, ey1, ez1 = self.A_IB1(t, q).T
         ex2, ey2, ez2 = self.A_IB2(t, q).T
-        
         J = np.hstack([self.J_R1(t, q), -self.J_R2(t, q)])
 
         W_g[:, 3] = cross3(ex1, ey2) @ J
         W_g[:, 4] = cross3(ey1, ez2) @ J
         W_g[:, 5] = cross3(ez1, ex2) @ J
         return W_g
+
+        # W_g_num = Numerical_derivative(self.g_dot, order=2)._y(t, q, np.zeros(self._nu)).T
+        # diff = W_g_num - W_g
+        # diff_error = diff
+        # error = np.linalg.norm(diff_error)
+        # print(f'error W_g: {error}')
+        # return W_g_num
 
     def W_g(self, t, q, coo):
         coo.extend(self.W_g_dense(t, q), (self.uDOF, self.la_gDOF))
@@ -236,6 +282,14 @@ class Rigid_connection():
                                 + np.einsum('ij,ik->kj', - la_g[5] * n_q2, J_R2)
 
         coo.extend( dense, (self.uDOF, self.qDOF))
+
+        # W_g_q = Numerical_derivative(self.W_g_dense, order=2)._x(t, q)
+        # dense_num = np.einsum('ijk,j->ik', W_g_q, la_g)
+        # diff = dense_num - dense
+        # diff_error = diff
+        # error = np.linalg.norm(diff_error)
+        # print(f'error Wla_g_q: {error}')
+        # coo.extend(dense_num, (self.uDOF, self.qDOF))
 
 class Rigid_connection2D(Rigid_connection):
     def __init__(self, *args, **kwargs):

cardillo/model/classical_beams/spatial/director.py
@@ -6,7 +6,7 @@ import os
 from cardillo.utility.coo import Coo
 from cardillo.discretization import uniform_knot_vector, B_spline_basis1D, Lagrange_basis
 from cardillo.discretization.B_spline import Knot_vector
-from cardillo.math.algebra import norm3, cross3, ax2skew, skew2ax, ax2skew_a
+from cardillo.math.algebra import norm3, cross3, ax2skew, skew2ax, skew2ax_A, ax2skew_a
 from cardillo.math.numerical_derivative import Numerical_derivative
 from cardillo.discretization.mesh1D import Mesh1D
 
@@ -614,7 +614,7 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
         # return A_IK_q_num
 
     def v_P(self, t, q, u, frame_ID, K_r_SP=None):
-        raise NotImplementedError('not tested!')
+        # raise NotImplementedError('not tested!')
         xi = frame_ID[0]
         if xi == 0:
             NN = self.N_bdry[0]
@@ -624,9 +624,9 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
             N, _ = self.basis_functions(frame_ID[0])
             NN = self.stack_shapefunctions(N)
         
-        v_P1 = NN @ u[self.rDOF] + self.A_IK(t, q, frame_ID) @ cross3(self.K_Omega(t, q, u, frame_ID=frame_ID), K_r_SP)
+        # v_P1 = NN @ u[self.rDOF] + self.A_IK(t, q, frame_ID) @ cross3(self.K_Omega(t, q, u, frame_ID=frame_ID), K_r_SP)
         v_P2 = NN @ u[self.rDOF] + self.A_IK(t, u, frame_ID) @ K_r_SP
-        print(v_P1 - v_P2)
+        # print(v_P1 - v_P2)
         return v_P2
 
     def v_P_q(self, t, q, u, frame_ID, K_r_SP=None):
@@ -639,7 +639,7 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
         return np.zeros((3, self.nq_el, self.nq_el))
 
     def a_P(self, t, q, u, u_dot, frame_ID, K_r_SP=None):
-        raise NotImplementedError('not tested!')
+        # raise NotImplementedError('not tested!')
         xi = frame_ID[0]
         if xi == 0:
             NN = self.N_bdry[0]
@@ -650,11 +650,11 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
             NN = self.stack_shapefunctions(N)
         
         # TODO: simplifications?
-        K_Omega = self.K_Omega(t, q, u, frame_ID=frame_ID)
-        K_Psi = self.K_Psi(t, q, u, u_dot, frame_ID=frame_ID)
-        a_P1 = NN @ u_dot[self.rDOF] + self.A_IK(t, q) @ (cross3(K_Psi, K_r_SP) + cross3(K_Omega, cross3(K_Omega, K_r_SP)))
-        a_P2 = NN @ u_dot[self.rDOF] + self.A_IK(t, u_dot, frame_ID) @ K_r_SP
-        print(a_P1 - a_P2)
+        # K_Omega = self.K_Omega(t, q, u, frame_ID=frame_ID)
+        # K_Psi = self.K_Psi(t, q, u, u_dot, frame_ID=frame_ID)
+        # a_P1 = NN @ u_dot[self.rDOF] + self.A_IK(t, q, frame_ID=frame_ID) @ (cross3(K_Psi, K_r_SP) + cross3(K_Omega, cross3(K_Omega, K_r_SP)))
+        a_P2 = NN @ u_dot[self.rDOF] + self.A_IK(t, u_dot, frame_ID=frame_ID) @ K_r_SP
+        # print(a_P1 - a_P2)
         return a_P2
 
     def a_P_q(self, t, q, u, u_dot, frame_ID, K_r_SP=None):
@@ -701,11 +701,20 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
         d3 = NN @ q[self.d3DOF]
         A_IK = np.vstack((d1, d2, d3)).T
 
+        K_Omega_tilde_Omega_tilde = skew2ax_A()
+
         K_J_R = np.zeros((3, self.nq_el))
-        K_J_R[:, self.d1DOF] = 0.5 * A_IK.T @ ax2skew(d1) @ NN
-        K_J_R[:, self.d2DOF] = 0.5 * A_IK.T @ ax2skew(d2) @ NN
-        K_J_R[:, self.d3DOF] = 0.5 * A_IK.T @ ax2skew(d3) @ NN
+        K_J_R[:, self.d1DOF] = K_Omega_tilde_Omega_tilde[0] @ A_IK.T @ NN
+        K_J_R[:, self.d2DOF] = K_Omega_tilde_Omega_tilde[1] @ A_IK.T @ NN
+        K_J_R[:, self.d3DOF] = K_Omega_tilde_Omega_tilde[2] @ A_IK.T @ NN
         return K_J_R
+
+        # K_J_R_num = Numerical_derivative(lambda t, q, u: self.K_Omega(t, q, u, frame_ID=frame_ID), order=2)._y(t, q, np.zeros_like(q))
+        # diff = K_J_R_num - K_J_R
+        # diff_error = diff
+        # error = np.linalg.norm(diff_error)
+        # print(f'error K_J_R: {error}')
+        # return K_J_R_num
 
     def K_J_R_q(self, t, q, frame_ID):
         xi = frame_ID[0]
@@ -717,25 +726,18 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
             N, _ = self.basis_functions(frame_ID[0])
             NN = self.stack_shapefunctions(N)
 
-        d1 = NN @ q[self.d1DOF]
-        d2 = NN @ q[self.d2DOF]
-        d3 = NN @ q[self.d3DOF]
-        A_IK = np.vstack((d1, d2, d3)).T
-
         A_IK_q = np.zeros((3, 3, self.nq_el))
         A_IK_q[:, 0, self.d1DOF] = NN
         A_IK_q[:, 1, self.d2DOF] = NN
         A_IK_q[:, 2, self.d3DOF] = NN
-        
+
+        K_Omega_tilde_Omega_tilde = skew2ax_A()
+        tmp = np.einsum('jil,jk->ikl', A_IK_q, NN)
+
         K_J_R_q = np.zeros((3, self.nq_el, self.nq_el))
-        di_tilde_qi = np.einsum('ijk,kl->ijl', ax2skew_a(), NN)
-        tmp = 0.5 * np.einsum('ij,jkm,kl->ilm', A_IK.T, di_tilde_qi, NN)
-        K_J_R_q[:, self.d1DOF] = 0.5 * np.einsum('jil,jk->ikl', A_IK_q, ax2skew(d1) @ NN)
-        K_J_R_q[:, self.d1DOF[:, None], self.d1DOF] += tmp
-        K_J_R_q[:, self.d2DOF] = 0.5 * np.einsum('jil,jk->ikl', A_IK_q, ax2skew(d2) @ NN)
-        K_J_R_q[:, self.d2DOF[:, None], self.d2DOF] += tmp
-        K_J_R_q[:, self.d3DOF] = 0.5 * np.einsum('jil,jk->ikl', A_IK_q, ax2skew(d3) @ NN)
-        K_J_R_q[:, self.d3DOF[:, None], self.d3DOF] += tmp
+        K_J_R_q[:, self.d1DOF] = np.einsum('ij,jkl->ikl', K_Omega_tilde_Omega_tilde[0], tmp)
+        K_J_R_q[:, self.d2DOF] = np.einsum('ij,jkl->ikl', K_Omega_tilde_Omega_tilde[1], tmp)
+        K_J_R_q[:, self.d3DOF] = np.einsum('ij,jkl->ikl', K_Omega_tilde_Omega_tilde[2], tmp)
         return K_J_R_q
 
         # K_J_R_q_num = Numerical_derivative(lambda t, q: self.K_J_R(t, q, frame_ID))._x(t, q)
@@ -744,7 +746,7 @@ class Timoshenko_beam_director(metaclass=ABCMeta):
         # return K_J_R_q_num
 
     def K_Psi(self, t, q, u, u_dot, frame_ID):
-        raise NotImplementedError('not tested!')
+        # raise NotImplementedError('not tested!')
         xi = frame_ID[0]
         if xi == 0:
             NN = self.N_bdry[0]
@@ -1141,9 +1143,10 @@ class Timoshenko_director_integral(Timoshenko_beam_director):
         self.g23DOF = np.arange(4*self.nn_el_g, 5*self.nn_el_g)
         self.g33DOF = np.arange(5*self.nn_el_g, 6*self.nn_el_g)
 
-    def __g_el(self, qe, N, N_g, J0, qw):
+    def __g_el(self, qe, el):
         g = np.zeros(self.nla_g_el)
 
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
         for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
             NNi = self.stack_shapefunctions(Ni)
 
@@ -1162,9 +1165,10 @@ class Timoshenko_director_integral(Timoshenko_beam_director):
 
         return g
 
-    def __g_q_el(self, qe, N, N_g, J0, qw):
+    def __g_q_el(self, qe, el):
         g_q = np.zeros((self.nla_g_el, self.nq_el))
 
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
         for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
             NNi = self.stack_shapefunctions(Ni)
 
@@ -1206,8 +1210,10 @@ class Timoshenko_director_integral(Timoshenko_beam_director):
         # print(f'error g_q: {error}')
         # return g_q_num
 
-    def __g_qq_el(self, qe, N, N_g, J0, qw):
+    def __g_qq_el(self, el):
         g_qq = np.zeros((self.nla_g_el, self.nq_el, self.nq_el))
+
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
         for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
             NNi = self.stack_shapefunctions(Ni)
             factor = NNi.T @ NNi * J0i * qwi
@@ -1244,35 +1250,199 @@ class Timoshenko_director_integral(Timoshenko_beam_director):
         # print(f'error g_qq: {error}')
         # return g_qq_num
 
+    def __g_dot_el(self, qe, ue, el):
+        g_dot = np.zeros(self.nla_g_el)
+
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
+        for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
+            NNi = self.stack_shapefunctions(Ni)
+
+            d1 = NNi @ qe[self.d1DOF]
+            d2 = NNi @ qe[self.d2DOF]
+            d3 = NNi @ qe[self.d3DOF]
+            d1_dot = NNi @ ue[self.d1DOF]
+            d2_dot = NNi @ ue[self.d2DOF]
+            d3_dot = NNi @ ue[self.d3DOF]
+
+            factor = N_gi * J0i * qwi
+         
+            g_dot[self.g11DOF] += 2 * d1 @ d1_dot * factor
+            g_dot[self.g12DOF] += (d1 @ d2_dot + d2 @ d1_dot) * factor
+            g_dot[self.g13DOF] += (d1 @ d3_dot + d3 @ d1_dot) * factor
+
+            g_dot[self.g22DOF] += 2 * d2 @ d2_dot * factor
+            g_dot[self.g23DOF] += (d2 @ d3_dot + d3 @ d2_dot) * factor
+
+            g_dot[self.g33DOF] += 2 * d3 @ d3_dot * factor
+
+        return g_dot
+        
+        # g_dot_num = Numerical_derivative(lambda t, q, u: self.__g_el(q, el))._t(0, qe, ue)
+        # g_dot_num += Numerical_derivative(lambda t, q, u: self.__g_el(q, el))._x(0, qe, ue) @ ue
+        # diff = g_dot_num - g_dot
+        # error = np.linalg.norm(diff)
+        # print(f'error g_dot: {error}')
+        # return g_dot_num
+
+    def __g_dot_q_el(self, qe, ue, el):
+        g_dot_q = np.zeros((self.nla_g_el, self.nq_el))
+
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
+        for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
+            NNi = self.stack_shapefunctions(Ni)
+
+            d1_dot = NNi @ ue[self.d1DOF]
+            d2_dot = NNi @ ue[self.d2DOF]
+            d3_dot = NNi @ ue[self.d3DOF]
+
+            factor = N_gi * J0i * qwi
+            outer1 = np.outer(factor, d1_dot @ NNi)
+            outer2 = np.outer(factor, d2_dot @ NNi)
+            outer3 = np.outer(factor, d3_dot @ NNi)
+         
+            # g_dot[self.g11DOF] += 2 * d1 @ d1_dot * factor
+            g_dot_q[self.g11DOF[:, None], self.d1DOF] += 2 * outer1
+            # g_dot[self.g12DOF] += (d1 @ d2_dot + d2 @ d1_dot) * factor
+            g_dot_q[self.g12DOF[:, None], self.d1DOF] += outer2
+            g_dot_q[self.g12DOF[:, None], self.d2DOF] += outer1
+            # g_dot[self.g13DOF] += (d1 @ d3_dot + d3 @ d1_dot) * factor
+            g_dot_q[self.g13DOF[:, None], self.d1DOF] += outer3
+            g_dot_q[self.g13DOF[:, None], self.d3DOF] += outer1
+
+            # g_dot[self.g22DOF] += 2 * d2 @ d2_dot * factor
+            g_dot_q[self.g22DOF[:, None], self.d2DOF] += 2 * outer2
+            # g_dot[self.g23DOF] += (d2 @ d3_dot + d3 @ d2_dot) * factor
+            g_dot_q[self.g23DOF[:, None], self.d2DOF] += outer3
+            g_dot_q[self.g23DOF[:, None], self.d3DOF] += outer2
+
+            # g_dot[self.g33DOF] += 2 * d3 @ d3_dot * factor
+            g_dot_q[self.g33DOF[:, None], self.d3DOF] += 2 * outer3
+
+        return g_dot_q
+
+        # g_dot_q_num = Numerical_derivative(lambda t, q, u: self.__g_dot_el(q, u, el), order=2)._x(0, qe, ue)
+        # diff = g_dot_q - g_dot_q_num
+        # error = np.linalg.norm(diff)
+        # print(f'error: {error}')
+        # return g_dot_q_num
+
+    def __g_ddot_el(self, qe, ue, ue_dot, el):
+        g_ddot = np.zeros(self.nla_g_el)
+
+        N, N_g , J0, qw = self.N[el], self.N_g[el], self.J0[el], self.qw[el]
+        for Ni, N_gi, J0i, qwi in zip(N, N_g, J0, qw):
+            NNi = self.stack_shapefunctions(Ni)
+
+            d1 = NNi @ qe[self.d1DOF]
+            d2 = NNi @ qe[self.d2DOF]
+            d3 = NNi @ qe[self.d3DOF]
+            d1_dot = NNi @ ue[self.d1DOF]
+            d2_dot = NNi @ ue[self.d2DOF]
+            d3_dot = NNi @ ue[self.d3DOF]
+            d1_ddot = NNi @ ue_dot[self.d1DOF]
+            d2_ddot = NNi @ ue_dot[self.d2DOF]
+            d3_ddot = NNi @ ue_dot[self.d3DOF]
+
+            factor = N_gi * J0i * qwi
+         
+            g_ddot[self.g11DOF] += 2 * (d1 @ d1_ddot + d1_dot @ d1_dot) * factor
+            g_ddot[self.g12DOF] += (d1 @ d2_ddot + 2 * d1_dot @ d2_dot + d2 @ d1_ddot) * factor
+            g_ddot[self.g13DOF] += (d1 @ d3_ddot + 2 * d1_dot @ d3_dot + d3 @ d1_ddot) * factor
+
+            g_ddot[self.g22DOF] += 2 * (d2 @ d2_ddot + d2_dot @ d2_dot) * factor
+            g_ddot[self.g23DOF] += (d2 @ d3_ddot + 2 * d2_dot @ d3_dot + d3 @ d2_ddot) * factor
+
+            g_ddot[self.g33DOF] += 2 * (d3 @ d3_ddot + d3_dot @ d3_dot) * factor
+
+        return g_ddot
+
+        # g_ddot_num = Numerical_derivative(lambda t, q, u: self.__g_dot_el(q, u, el))._t(0, qe, ue)
+        # g_ddot_num += Numerical_derivative(lambda t, q, u: self.__g_dot_el(q, u, el))._x(0, qe, ue) @ ue
+        # g_ddot_num += Numerical_derivative(lambda t, q, u: self.__g_dot_el(q, u, el))._y(0, qe, ue) @ ue_dot
+        # diff = g_ddot_num - g_ddot
+        # error = np.linalg.norm(diff)
+        # print(f'error g_ddot: {error}')
+        # return g_ddot_num
+
+    def __g_ddot_q_el(self, qe, ue, ue_dot, el):
+        return Numerical_derivative(lambda t, q, u: self.__g_ddot_el(q, u, ue_dot, el), order=2)._x(0, qe, ue)
+
+    def __g_ddot_u_el(self, qe, ue, ue_dot, el):
+        return Numerical_derivative(lambda t, q, u: self.__g_ddot_el(q, u, ue_dot, el), order=2)._y(0, qe, ue)
+
     # global constraint functions
     def g(self, t, q):
         g = np.zeros(self.nla_g)
         for el in range(self.nEl):
             elDOF = self.elDOF[el]
             elDOF_g = self.elDOF_g[el]
-            g[elDOF_g] += self.__g_el(q[elDOF], self.N[el], self.N_g[el], self.J0[el], self.qw[el])
+            g[elDOF_g] += self.__g_el(q[elDOF], el)
         return g
 
     def g_q(self, t, q, coo):
         for el in range(self.nEl):
             elDOF = self.elDOF[el]
             elDOF_g = self.elDOF_g[el]
-            g_q = self.__g_q_el(q[elDOF], self.N[el], self.N_g[el], self.J0[el], self.qw[el])
+            g_q = self.__g_q_el(q[elDOF], el)
             coo.extend(g_q, (self.la_gDOF[elDOF_g], self.qDOF[elDOF]))
 
     def W_g(self, t, q, coo):
         for el in range(self.nEl):
             elDOF = self.elDOF[el]
             elDOF_g = self.elDOF_g[el]
-            g_q = self.__g_q_el(q[elDOF], self.N[el], self.N_g[el], self.J0[el], self.qw[el])
+            g_q = self.__g_q_el(q[elDOF], el)
             coo.extend(g_q.T, (self.uDOF[elDOF], self.la_gDOF[elDOF_g]))
 
     def Wla_g_q(self, t, q, la_g, coo):
         for el in range(self.nEl):
             elDOF = self.elDOF[el]
             elDOF_g = self.elDOF_g[el]
-            g_qq = self.__g_qq_el(q[elDOF], self.N[el], self.N_g[el], self.J0[el], self.qw[el])
+            g_qq = self.__g_qq_el(el)
             coo.extend(np.einsum('i,ijk->jk', la_g[elDOF_g], g_qq), (self.uDOF[elDOF], self.qDOF[elDOF]))
+
+    def g_dot(self, t, q, u):
+        g_dot = np.zeros(self.nla_g)
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_dot[elDOF_g] += self.__g_dot_el(q[elDOF], u[elDOF], el)
+        return g_dot
+
+    def g_dot_q(self, t, q, u, coo):
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_dot_q = self.__g_dot_q_el(q[elDOF], u[elDOF], el)
+            coo.extend(g_dot_q, (self.la_gDOF[elDOF_g], self.qDOF[elDOF]))  
+
+    def g_dot_u(self, t, q, coo):
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_dot_u = self.__g_q_el(q[elDOF], el)
+            coo.extend(g_dot_u, (self.la_gDOF[elDOF_g], self.uDOF[elDOF]))  
+
+    def g_ddot(self, t, q, u, u_dot):
+        g_ddot = np.zeros(self.nla_g)
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_ddot[elDOF_g] += self.__g_ddot_el(q[elDOF], u[elDOF], u_dot[elDOF], el)
+        return g_ddot
+
+    def g_ddot_q(self, t, q, u, u_dot, coo):
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_ddot_q = self.__g_ddot_q_el(q[elDOF], u[elDOF], u_dot[elDOF], el)
+            coo.extend(g_ddot_q, (self.la_gDOF[elDOF_g], self.qDOF[elDOF]))  
+
+    def g_ddot_u(self, t, q, u, u_dot, coo):
+        for el in range(self.nEl):
+            elDOF = self.elDOF[el]
+            elDOF_g = self.elDOF_g[el]
+            g_ddot_u = self.__g_ddot_u_el(q[elDOF], u[elDOF], u_dot[elDOF], el)
+            coo.extend(g_ddot_u, (self.la_gDOF[elDOF_g], self.uDOF[elDOF]))  
 
 class Euler_Bernoulli_director_integral(Timoshenko_beam_director):
     def __init__(self, *args, **kwargs):

examples/Wilberforce_pendulum.py
@@ -149,50 +149,6 @@ def helix3D(t, r, c, plane='xyz'):
 
     return P, dP, ddP
 
-def helix_spring3D(n_turns, pitch, r, n_points, plane='xyz'):
-    """Helix function in xyz plane, see https://mathworld.wolfram.com/Helix.html
-    """
-    print('this function does not work!')
-    orientation = 'xyz'
-    perm = []
-    for i in plane:
-        perm.append(orientation.find(i))
-
-    n_start = n_end = int(0.25 * n_points / n_turns)
-    n_cricle = n_points - n_start - n_end
-
-    # end of the helix shaped spring
-    phi0 = np.linspace(-0.5, 0, n_start, endpoint=False)
-    # phi0 = np.linspace(-np.pi, 0, n_start, endpoint=False)
-    P0, dP0, ddP0 = helix3D(phi0, r / 2, 0.5 * pitch, plane=plane)
-    z_min = P0[:, 2].min()
-    P0[:, perm[0]] += r / 2
-
-    phi1 = np.linspace(0, n_turns, n_cricle, endpoint=False)
-    P1, dP1, ddP1 = helix3D(phi1, r, pitch, plane=plane)
-
-    phi2 = np.linspace(0, 0.5, n_end, endpoint=True)
-    P2, dP2, ddP2 = helix3D(phi2, r / 2, 0.5 * pitch, plane=plane)
-    P2[:, perm[0]] += r / 2
-    P2[:, perm[2]] += pitch * phi1[-1]
-
-    # combine all three parts of the spring
-    P = np.concatenate([P0, P1, P2])
-    P[:, perm[2]] -= z_min
-    dP = np.concatenate([dP0, dP1, dP2])
-    ddP = np.concatenate([ddP0, ddP1, ddP2])
-    
-    # P = np.concatenate([P0, P1])
-    # P[:, perm[2]] -= z_min
-    # dP = np.concatenate([dP0, dP1])
-    # ddP = np.concatenate([ddP0, ddP1])
-    
-    # P = P1
-    # dP = dP1
-    # ddP = ddP1
-
-    return P, dP, ddP
-
 # Beam = Timoshenko_director_dirac
 Beam = Timoshenko_director_integral
 # Beam = Euler_Bernoulli_director_integral
@@ -201,8 +157,8 @@ Beam = Timoshenko_director_integral
 # statics = True
 statics = False
 
-# save = True
-save = False
+save = True
+# save = False
 
 import os
 path = os.path.dirname(os.path.abspath(__file__))
@@ -239,16 +195,16 @@ if __name__ == "__main__":
     ###########################
     # discretization properties
     ###########################
-    # p = 1
     # p = 2
     p = 3
     # nQP = p + 1
     nQP = int(np.ceil((p**2 + 1) / 2)) + 1 # dynamics
     print(f'nQP: {nQP}')
-    # nEl = 16 # 2 turns
+    # nEl = 4 # 1 turn
+    nEl = 16 # 2 turns
     # nEl = 32 # 5 turns
     # nEl = 64 # 10 turns
-    nEl = 128 # 20 turns
+    # nEl = 128 # 20 turns
 
     #############################
     # fit reference configuration
@@ -256,10 +212,11 @@ if __name__ == "__main__":
     coil_diameter = 32.0e-3 # 32mm
     coil_radius = coil_diameter / 2
     pitch_unloaded = 1.0e-3 # 1mm
-    # turns = 2
+    # turns = 0.5
+    turns = 2
     # turns = 5
     # turns = 10
-    turns = 20
+    # turns = 20
     nxi = 1000
 
     xi = np.linspace(0, turns, nxi)
@@ -331,9 +288,9 @@ if __name__ == "__main__":
     max_iter = 30
     tol = 1.0e-6
 
-    t1 = 10
+    t1 = 1
     # dt = 1.0e-2 # beam as static force element implicit Euler
-    dt = 1e-3 # full beam dynamics generalized alpha
+    dt = 5e-3 # full beam dynamics generalized alpha
     # dt = 5e-4 # full beam dynamics generalized alpha
 
     beam = Beam(material_model, A_rho0, B_rho0, C_rho0, p, nQP, nEl, q0=Q, Q=Q)
@@ -367,7 +324,8 @@ if __name__ == "__main__":
         # uDOF_algebraic = beam.uDOF # beam as static force element
         # solver = Euler_backward_singular(model, t1, dt, uDOF_algebraic=uDOF_algebraic, numerical_jacobian=False, debug=False, newton_max_iter=20)
 
-        solver = Generalized_alpha_4(model, t1, dt, rho_inf=0.75, uDOF_algebraic=uDOF_algebraic, newton_tol=1.0e-6)
+        # solver = Generalized_alpha_4(model, t1, dt, rho_inf=0.75, uDOF_algebraic=uDOF_algebraic, newton_tol=1.0e-6, numerical_jacobian=False)
+        solver = Generalized_alpha_4(model, t1, dt, rho_inf=0.5, uDOF_algebraic=uDOF_algebraic, newton_tol=1.0e-6, numerical_jacobian=False)
         
     export_path = f'Wilberforce_pendulum_p{p}_nEL{nEl}_turns{turns}_t1{t1}_dt{dt}_c{c}'
 
@@ -395,22 +353,24 @@ if __name__ == "__main__":
     #####################################
     # fig = plt.figure()
     fig = plt.figure(figsize=(10, 6))
-    ax1 = fig.add_subplot(1, 3, 1, projection='3d')
-    ax2 = fig.add_subplot(1, 3, 2)
-    ax3 = fig.add_subplot(1, 3, 3)
+    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax3 = ax2.twinx()
 
-    # visualize bob's displacements
+    # # visualize bob's displacements
     q_bob = q[:, bob.qDOF]
-    ax2.plot(t, q_bob[:, 0], '-k', label='x')
-    ax2.plot(t, q_bob[:, 1], '--k', label='y')
-    ax2.plot(t, q_bob[:, 2], '-.k', label='z')
+    ax2.plot(t, q_bob[:, 0], '-b', label='x')
+    ax2.plot(t, q_bob[:, 1], '--b', label='y')
+    ax2.plot(t, q_bob[:, 2], '-.b', label='z')
+    ax2.tick_params(axis='y', labelcolor='blue')
     ax2.grid(True)
     ax2.legend()
 
     # visualize brotation angles
-    ax3.plot(t, q_bob[:, 3] * 180 / np.pi, '-k', label='phi_z')
-    ax3.plot(t, q_bob[:, 4] * 180 / np.pi, '--k', label='phi_x')
-    ax3.plot(t, q_bob[:, 5] * 180 / np.pi, '-.k', label='phi_y')
+    ax3.plot(t, q_bob[:, 3] * 180 / np.pi, '-r', label='phi_z')
+    ax3.plot(t, q_bob[:, 4] * 180 / np.pi, '--r', label='phi_x')
+    ax3.plot(t, q_bob[:, 5] * 180 / np.pi, '-.r', label='phi_y')
+    ax3.tick_params(axis='y', labelcolor='red')
     ax3.grid(True)
     ax3.legend()
     
@@ -421,10 +381,10 @@ if __name__ == "__main__":
     length_directors = 1.0e-2
     ax1.set_xlim3d(left=-scale, right=scale)
     ax1.set_ylim3d(bottom=-scale, top=scale)
-    ax1.set_zlim3d(bottom=-scale, top=scale)
+    ax1.set_zlim3d(bottom=-2*scale, top=0)
 
     # prepare data for animation    
-    slowmotion = 1
+    slowmotion = 10
     fps = 10
     animation_time = slowmotion * t1
     target_frames = int(fps * animation_time)
@@ -445,15 +405,21 @@ if __name__ == "__main__":
     d2_, = ax1.plot([], [], [], '-g')
     d3_, = ax1.plot([], [], [], '-b')
 
+    y_, = ax2.plot([], [], 'ob')
+    phi_z_, = ax3.plot([], [], 'or')
+
     def animate(i):
+        # animate beam centerline
         x, y, z = beam.centerline(q[i], n=400)
         center_line.set_data(x, y)
         center_line.set_3d_properties(z)
 
-        # x, y, z = q[i][beam.qDOF].reshape(12, -1)[:3]
-        # nodes.set_data(x, y)
-        # nodes.set_3d_properties(z)
+        # animate beam nodes
+        x, y, z = q[i][beam.qDOF].reshape(12, -1)[:3]
+        nodes.set_data(x, y)
+        nodes.set_3d_properties(z)
 
+        # animate rigid body
         x_S, y_S, z_S = bob.r_OP(t[i], q[i][bob.qDOF])    
         bob_com.set_data(np.array([x_S]), np.array([y_S]))
         bob_com.set_3d_properties(np.array([z_S]))
@@ -469,9 +435,11 @@ if __name__ == "__main__":
         d3_.set_data(np.array([x_S, x_S + d3[0]]), np.array([y_S, y_S + d3[1]]))
         d3_.set_3d_properties(np.array([z_S, z_S + d3[2]]))
 
-        return center_line, nodes, bob_com, d1_, d2_, d3_
+        # animate y and phi_z
+        y_.set_data(np.array([t[i]]), np.array([z_S]))
+        phi_z_.set_data(np.array([t[i]]), np.array([q[i, bob.qDOF[3]] * 180 / np.pi]))
 
-    animate(1)
+        return center_line, nodes, bob_com, d1_, d2_, d3_, y_, phi_z_
 
     anim = animation.FuncAnimation(fig, animate, frames=frames, interval=interval, blit=False)
     plt.show()

examples/rolling_disc.py
@@ -16,9 +16,9 @@ from cardillo.model.force import Force
 from cardillo.solver import Euler_backward, Moreau, Moreau_sym, Scipy_ivp, Generalized_alpha_1, Generalized_alpha_2, Generalized_alpha_3, Generalized_alpha_4, Generalized_alpha_5
 from cardillo.math.algebra import axis_angle2quat, ax2skew, A_IK_basic_x
 
-# rigid_body = 'Euler'
+rigid_body = 'Euler'
 # rigid_body = 'Quaternion'
-rigid_body = 'Director'
+# rigid_body = 'Director'
 # rigid_body = 'Director2'
 
 class Rigid_disc_euler(Rigid_body_euler):
@@ -300,16 +300,16 @@ def rolling_disc_velocity_constraints():
     model.assemble()
 
     t0 = 0
-    t1 = 2 * np.pi / np.abs(alpha_dot)
-    dt = 2.5e-2
+    t1 = 2 * np.pi / np.abs(alpha_dot) * 0.25
+    dt = 5e-3
     # solver = Euler_backward(model, t1, dt, numerical_jacobian=False, debug=False)
-    # solver = Moreau_sym(model, t1, dt, numerical_jacobian=False, debug=False)
+    # solver = Moreau_sym(model, t1, dt)
     # solver = Generalized_alpha_1(model, t1, variable_dt=True, rtol=0, atol=1.0e-2)
     # solver = Generalized_alpha_1(model, t1, dt=dt, variable_dt=False)
     # solver = Generalized_alpha_2(model, t1, dt)
     # solver = Generalized_alpha_3(model, t1, dt, numerical_jacobian=True)
     # solver = Generalized_alpha_3(model, t1, dt, numerical_jacobian=False)
-    solver = Generalized_alpha_4(model, t1, dt, numerical_jacobian=True)
+    solver = Generalized_alpha_4(model, t1, dt, numerical_jacobian=False)
     # solver = Generalized_alpha_5(model, t1, dt, numerical_jacobian=False)
     # solver = Moreau(model, t1, dt)
     # solver = Scipy_ivp(model, t1, dt, atol=1.e-6, method='RK23')