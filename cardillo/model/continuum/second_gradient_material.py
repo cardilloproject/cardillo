a7ad31b887271ee35fa6bee0d6825238d3633880
maxstilz <maximilian.stilz@inatech.uni-freiburg.de>
2022-02-23 18:30:52 +0100

    second_gardient routine

---
+++ cardillo/model/continuum/second_gradient_material.py
@@ -0,0 +1,7 @@
+# from abc import ABC, abstractmethod
+import numpy as np
+from math import sqrt, log, isclose
+from cardillo.math.algebra import determinant2D, determinant3D, inverse3D
+from cardillo.math.numerical_derivative import Numerical_derivative
+
+class Pantobox_()
