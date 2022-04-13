Corrected curvature computation in skew coordinates and minro changes.

cardillo/beams/spatial/DirectorAxisAngle.py
@@ -559,8 +559,8 @@ class DirectorAxisAngle:
         f = np.zeros(self.nu)
         for el in range(self.nelement):
             elDOF = self.elDOF[el]
-            # f[elDOF] += self.f_pot_el(q[elDOF], el)
-            f[elDOF] += self.f_pot_el_test(q[elDOF], el)
+            f[elDOF] += self.f_pot_el(q[elDOF], el)
+            # f[elDOF] += self.f_pot_el_test(q[elDOF], el)
         # self.f_pot_el_test(q[elDOF], el)
         return f
 
@@ -616,14 +616,14 @@ class DirectorAxisAngle:
             # formulation of Harsch2020b
             #################################################################
 
-            # torsional and flexural strains
-            K_Kappa = np.array(
-                [
-                    0.5 * (d3 @ d2_s - d2 @ d3_s),
-                    0.5 * (d1 @ d3_s - d3 @ d1_s),
-                    0.5 * (d2 @ d1_s - d1 @ d2_s),
-                ]
-            )
+            # # torsional and flexural strains
+            # K_Kappa = np.array(
+            #     [
+            #         0.5 * (d3 @ d2_s - d2 @ d3_s),
+            #         0.5 * (d1 @ d3_s - d3 @ d1_s),
+            #         0.5 * (d2 @ d1_s - d1 @ d2_s),
+            #     ]
+            # )
 
             ###################################################
             # formulation in skew coordinates, see Eugster2014c
@@ -636,15 +636,15 @@ class DirectorAxisAngle:
             # TODO: Investiage why rotation of left beam end is not working
             # with this formulation.
 
-            # # torsional and flexural strains
-            # d = d1 @ cross3(d2, d3)
-            # K_Kappa = np.array(
-            #     [
-            #         0.5 * (d3 @ d2_s - d2 @ d3_s) / d,
-            #         0.5 * (d1 @ d3_s - d3 @ d1_s) / d,
-            #         0.5 * (d2 @ d1_s - d1 @ d2_s) / d,
-            #     ]
-            # )
+            # torsional and flexural strains
+            d = d1 @ cross3(d2, d3)
+            K_Kappa = np.array(
+                [
+                    0.5 * (d3 @ d2_s - d2 @ d3_s) / d,
+                    0.5 * (d1 @ d3_s - d3 @ d1_s) / d,
+                    0.5 * (d2 @ d1_s - d1 @ d2_s) / d,
+                ]
+            )
 
             # compute contact forces and couples from partial derivatives of
             # the strain energy function w.r.t. strain measures
@@ -666,11 +666,11 @@ class DirectorAxisAngle:
             # - delta kappa part
             fe[self.psiDOF] += -NN_psi_xii.T @ K_m * qwi
             fe[self.psiDOF] += (
-                NN_psi_i.T
-                @ cross3(K_Kappa, K_m)
-                * Ji
-                * qwi
-                # NN_psi_i.T @ cross3(K_Kappa, K_m) * Ji * d * qwi
+                # NN_psi_i.T
+                # @ cross3(K_Kappa, K_m)
+                # * Ji
+                # * qwi
+                NN_psi_i.T @ cross3(K_Kappa, K_m) * Ji * d * qwi
             )  # Euler term
 
         return fe
@@ -682,6 +682,13 @@ class DirectorAxisAngle:
 
             # sparse assemble element internal stiffness matrix
             coo.extend(Ke, (self.uDOF[elDOF], self.qDOF[elDOF]))
+
+        # from scipy.sparse import csc_matrix
+        # sparse = coo.tosparse(csc_matrix)
+        # dense = sparse.toarray()
+        # print(f"f_pot_q.shape: {dense.shape}")
+        # print(f"f_pot_q.rank: {np.linalg.matrix_rank(dense)}")
+        # print(f"")
 
     # def f_pot_q_el(self, qe, el):
     #     return approx_fprime(qe, lambda qe: self.f_pot_el(qe, el), method="2-point")
@@ -761,7 +768,7 @@ class DirectorAxisAngle:
 
     # Interpolation of nodal rotations
     def f_pot_q_el(self, qe, el):
-        # return approx_fprime(qe, lambda qe: self.f_pot_el(qe, el), method="2-point")
+        return approx_fprime(qe, lambda qe: self.f_pot_el(qe, el), method="2-point")
 
         Ke = np.zeros((self.nq_element, self.nq_element))
 
@@ -1075,14 +1082,14 @@ class DirectorAxisAngle:
     # TODO:
     def A_IK_q(self, t, q, frame_ID):
         return approx_fprime(q, lambda q: self.A_IK(t, q, frame_ID))
-        N, _ = self.basis_functions_psi(frame_ID[0])
-        NN = self.stack3psi(N)
+        # N, _ = self.basis_functions_psi(frame_ID[0])
+        # NN = self.stack3psi(N)
 
-        A_IK_q = np.zeros((3, 3, self.nq_element))
-        A_IK_q[:, 0, self.psiDOF] = NN
-        A_IK_q[:, 1, self.d2DOF] = NN
-        A_IK_q[:, 2, self.d3DOF] = NN
-        return A_IK_q
+        # A_IK_q = np.zeros((3, 3, self.nq_element))
+        # A_IK_q[:, 0, self.psiDOF] = NN
+        # A_IK_q[:, 1, self.d2DOF] = NN
+        # A_IK_q[:, 2, self.d3DOF] = NN
+        # return A_IK_q
 
     def v_P(self, t, q, u, frame_ID, K_r_SP=np.zeros(3)):
         N, _ = self.basis_functions_r(frame_ID[0])
@@ -1093,9 +1100,9 @@ class DirectorAxisAngle:
         )
         return v_P
 
-    # TODO:
     def v_P_q(self, t, q, u, frame_ID, K_r_SP=np.zeros(3)):
-        raise NotImplementedError("")
+        K_Omega = self.K_Omega(t, q, u, frame_ID)
+        return np.einsum("ijk,j->ik", self.A_IK_q(t, q), cross3(K_Omega, K_r_SP))
 
     # TODO:
     def J_P(self, t, q, frame_ID, K_r_SP=np.zeros(3)):
@@ -1139,10 +1146,22 @@ class DirectorAxisAngle:
         # return approx_fprime(
         #     np.zeros(self.nq_element), lambda u: self.K_Omega(t, q, u, frame_ID)
         # )
+
+        # A_IK = self.A_IK(t, q, frame_ID)
+        # d1, d2, d3 = A_IK.T
+        # #fmt: off
+        # d_ij = np.array([
+        #     [d1 @ d1, d1 @ d2, d1 @ d3],
+        #     [d2 @ d1, d2 @ d2, d2 @ d3],
+        #     [d3 @ d1, d3 @ d2, d3 @ d3]
+        # ])
+        # #fmt: on
+
         N, _ = self.basis_functions_psi(frame_ID[0])
         NN = self.stack3psi(N)
         K_J_R = np.zeros((3, self.nq_element))
         K_J_R[:, self.psiDOF] = NN
+        # K_J_R[:, self.psiDOF] = d_ij @ NN
         return K_J_R
 
     def K_J_R_q(self, t, q, frame_ID):

cardillo/math/rotations.py
@@ -228,10 +228,22 @@ def smallest_rotation(a0: np.ndarray, a: np.ndarray) -> np.ndarray:
     a0_bar = a0 / norm(a0)
     a_bar = a / norm(a)
     e = cross3(a0_bar, a_bar)
-    e_tilde = ax2skew(e)
-    return (
-        np.eye(3) + e_tilde + (e_tilde @ e_tilde) / (1 + a0_bar @ a_bar)
-    )  # Crisfield1996 (16.104)
+
+    # ########################
+    # # Crisfield1996 (16.104)
+    # ########################
+    # e_tilde = ax2skew(e)
+    # return np.eye(3) + e_tilde + (e_tilde @ e_tilde) / (1 + a0_bar @ a_bar)
+
+    ########################
+    # Crisfield1996 (16.105)
+    ########################
+    cos_psi = a0_bar @ a_bar
+    denom = 1 + cos_psi
+    if denom > 0:
+        return cos_psi * np.eye(3) + ax2skew(e) + np.outer(e, e) / denom
+    else:
+        return cos_psi * np.eye(3)
 
 
 ##########################################
@@ -279,3 +291,28 @@ def quat2rot_p(p):
 def axis_angle2quat(axis, angle):
     n = axis / norm(axis)
     return np.concatenate([[cos(angle / 2)], sin(angle / 2) * n])
+
+
+if __name__ == "__main__":
+    from cardillo.math import e1, e2, e3, pi, sin, cos
+
+    a0 = e1
+    a = np.random.rand(3)
+    a_fun = lambda t: cos(t) * e1 +  sin(t) * e2
+
+    num = 5
+    ts = np.linspace(0, 1.0 * pi, num=num)
+
+    as_ = np.array([a_fun(t) for t in ts])
+
+    Rs = np.array([
+        smallest_rotation(a0, a) for a in as_
+    ]).transpose(1, 2, 0)
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(projection="3d"))
+    ax.quiver(*np.zeros((3, num)), *Rs[:, 0], color="red")
+    ax.quiver(*np.zeros((3, num)), *Rs[:, 1], color="green")
+    ax.quiver(*np.zeros((3, num)), *Rs[:, 2], color="blue")
+    plt.show()

examples/Kirchhoff.py
@@ -63,7 +63,7 @@ def beam_factory(
         )
     elif Beam == DirectorAxisAngle:
         p_r = polynomial_degree
-        p_psi = p_r  # - 1
+        p_psi = p_r
         Q = DirectorAxisAngle.straight_configuration(
             p_r, p_psi, nelements, L, r_OP=r_OP, A_IK=A_IK, basis=shape_functions
         )
@@ -172,14 +172,14 @@ def run(statics=True):
     Beam = DirectorAxisAngle
 
     # number of elements
-    # nelements = 3
+    # nelements = 5
     nelements = 10
     # nelements = 20
-    # nelements = 50
+    # nelements = 30
 
     # used polynomial degree
-    # polynomial_degree = 1
-    polynomial_degree = 3
+    polynomial_degree = 1
+    # polynomial_degree = 3
 
     # number of quadrature points
     # nquadrature_points = int(np.ceil((polynomial_degree + 1)**2 / 2))
@@ -256,12 +256,12 @@ def run(statics=True):
     # A_IK0 = lambda t: A_IK_basic(phi(t)).x()
     # TODO: Get this strange rotation working with a full circle
     # A_IK0 = lambda t: A_IK_basic(phi(t)).z()
-    # A_IK0 = (
-    #     lambda t: A_IK_basic(0.5 * phi(t)).z()
-    #     @ A_IK_basic(0.5 * phi(t)).y()
-    #     @ A_IK_basic(phi(t)).x()
-    # )
-    A_IK0 = lambda t: np.eye(3)
+    A_IK0 = (
+        lambda t: A_IK_basic(0.5 * phi(t)).z()
+        @ A_IK_basic(0.5 * phi(t)).y()
+        @ A_IK_basic(phi(t)).x()
+    )
+    # A_IK0 = lambda t: np.eye(3)
     frame1 = Frame(r_OP=r_OB0, A_IK=A_IK0)
 
     # left and right joint
@@ -278,9 +278,9 @@ def run(statics=True):
 
     # moment at right end
     Fi = material_model.Fi
-    # M = lambda t: -np.array([1, 0, 1]) * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[1] / L * 1.0
+    M = lambda t: np.array([1, 1, 0]) * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[1] / L
     # M = lambda t: e1 * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[0] / L * 1.0
-    M = lambda t: e2 * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[1] / L
+    # M = lambda t: e2 * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[1] / L * 0.9
     # M = lambda t: e3 * smoothstep2(t, 0.0, frac_deformation) * 2 * np.pi * Fi[2] / L * 1.0
     moment = K_Moment(M, beam, (1,))
 