731a054e4d9eb499e1f73b56ba6a846586b60dde
Jonas Harsch <jonas_harsch@web.de>
2022-06-30 15:05:27 +0200

    All examples of the paper are working with the special beam implementation + additional buckling example.

--- examples/Harsch2022b.py
+++ examples/Harsch2022b.py
@@ -511,6 +511,244 @@ def convergence_quater_circle():
     plt.show()
 
 
+def HelixIbrahimbegovic1997():
+    """Beam bent to a helical form - Section 5.2 of Ibrahimbegovic1997.
+
+    References
+    ==========
+    Ibrahimbegovic1997: https://doi.org/10.1016/S0045-7825(97)00059-5
+    """
+    # fraction of 10 full rotations and the out of plane force
+    # a corresponding fraction of 100 elements is chosen
+    # # fraction = 0.05
+    fraction = 0.1  # 1 full rotations
+    # fraction = 0.20  # 2 full rotations
+    # fraction = 0.4  # 4 full rotations
+    # fraction = 0.5  # 5 full rotations
+    # fraction = 1  # 10 full rotations
+
+    # number of elements
+    nelements_max = 30
+    nelements = max(3, int(fraction * nelements_max))
+    print(f"nelemen: {nelements}")
+
+    # used polynomial degree
+    polynomial_degree = 1
+
+    # beam parameters found in Section 5.1 Ibrahimbegovic1997
+    L = 10
+    EA = GA = 1.0e4
+    GJ = EI = 1.0e2
+
+    # build quadratic material model
+    Ei = np.array([EA, GA, GA], dtype=float)
+    Fi = np.array([GJ, EI, EI], dtype=float)
+    material_model = Simo1986(Ei, Fi)
+
+    # Note: This is never used in statics!
+    line_density = 1.0
+    radius = 1.0
+    cross_section = CircularCrossSection(line_density, radius)
+    A_rho0 = line_density * cross_section.area
+    K_S_rho0 = line_density * cross_section.first_moment
+    K_I_rho0 = line_density * cross_section.second_moment
+
+    # starting point and orientation of initial point, initial length
+    r_OP0 = np.zeros(3, dtype=float)
+    A_IK0 = np.eye(3, dtype=float)
+
+    q0 = TimoshenkoAxisAngleSE3.straight_configuration(
+        polynomial_degree,
+        nelements,
+        L,
+        r_OP=r_OP0,
+        A_IK=A_IK0,
+    )
+    beam = TimoshenkoAxisAngleSE3(
+        polynomial_degree,
+        material_model,
+        A_rho0,
+        K_S_rho0,
+        K_I_rho0,
+        nelements,
+        q0,
+    )
+
+    # junctions
+    frame1 = Frame(r_OP=r_OP0, A_IK=A_IK0)
+
+    # left and right joint
+    joint1 = RigidConnection(frame1, beam, frame_ID2=(0,))
+
+    # moment at right end
+    Fi = material_model.Fi
+    M = lambda t: (e3 * 10 * 2 * np.pi * Fi[2] / L * t * fraction)
+    moment = Moment(M, beam, (1,))
+
+    # external force at the right end
+    F_max = 50  # Ibrahimbegovic1997
+    F = lambda t: F_max * e3 * t * fraction
+    force = Force(F, beam, frame_ID=(1,))
+
+    # assemble the model
+    model = Model()
+    model.add(beam)
+    model.add(frame1)
+    model.add(joint1)
+    model.add(moment)
+    model.add(force)
+    model.assemble()
+
+    n_load_steps = int(200 * 10 * fraction)  # works for all cases
+
+    solver = Newton(
+        model,
+        n_load_steps=n_load_steps,
+        max_iter=30,
+        atol=1.0e-8,
+        numerical_jacobian=False,
+    )
+    sol = solver.solve()
+    q = sol.q
+    nt = len(q)
+    t = sol.t[:nt]
+
+    #########################
+    # export tip displacement
+    #########################
+    header = "t, x, y, z"
+    frame_ID = (1,)
+    elDOF = beam.qDOF_P(frame_ID)
+    r_OC_L = np.array([beam.r_OP(ti, qi[elDOF], frame_ID) for (ti, qi) in zip(t, q)])
+    # export_data = np.vstack([t, *r_OC_L.T]).T
+    export_data = np.vstack([np.arange(nt), *r_OC_L.T]).T
+    np.savetxt(
+        "results/tip_displacement_helix.txt",
+        export_data,
+        delimiter=", ",
+        header=header,
+        comments="",
+    )
+
+    ###################
+    # export centerline
+    ###################
+    nframes = 37
+    idxs = np.linspace(0, nt - 1, num=nframes, dtype=int)
+    for i, idx in enumerate(idxs):
+        ti = t[idx]
+        qi = q[idx]
+        r_OPs = beam.centerline(qi, n=500)
+
+        header = "x, y, z"
+        np.savetxt(
+            f"results/centerline{i}.txt",
+            r_OPs.T,
+            delimiter=", ",
+            header=header,
+            comments="",
+        )
+
+    ########################
+    # export strain measures
+    ########################
+    nxi = 200
+    xis = np.linspace(0.0, 1.0, num=nxi, dtype=float)
+
+    K_Gamma = np.zeros((3, nxi), dtype=float)
+    K_Kappa = np.zeros((3, nxi), dtype=float)
+    for i, xi in enumerate(xis):
+        elDOF = beam.qDOF_P((xi,))
+
+        _, _, K_Gamma_bar, K_Kappa_bar = beam.eval(q[0, beam.qDOF[elDOF]], xi)
+        J = norm(K_Gamma_bar)
+
+        _, _, K_Gamma_bar, K_Kappa_bar = beam.eval(q[-1, beam.qDOF[elDOF]], xi)
+        K_Gamma[:, i] = K_Gamma_bar / J
+        K_Kappa[:, i] = K_Kappa_bar / J
+
+    header = "xi, K_Gamma0, K_Gamma1, K_Gamma2"
+    export_data = np.c_[xis, K_Gamma.T]
+    np.savetxt(
+        f"results/K_Gamma.txt",
+        export_data,
+        delimiter=", ",
+        header=header,
+        comments="",
+    )
+
+    header = "xi, K_Kappa0, K_Kappa1, K_Kappa2"
+    export_data = np.c_[xis, K_Kappa.T]
+    np.savetxt(
+        f"results/K_Kappa.txt",
+        export_data,
+        delimiter=", ",
+        header=header,
+        comments="",
+    )
+
+    ##################################
+    # visualize nodal rotation vectors
+    ##################################
+    fig, ax = plt.subplots()
+
+    for i, nodalDOF_psi in enumerate(beam.nodalDOF_psi):
+        psi = q[:, beam.qDOF[nodalDOF_psi]]
+        ax.plot(t, np.linalg.norm(psi, axis=1), label=f"||psi{i}||")
+
+    ax.set_xlabel("t")
+    ax.set_ylabel("nodal rotation vectors")
+    ax.grid()
+    ax.legend()
+
+    ################################
+    # visualize norm strain measures
+    ################################
+    fig, ax = plt.subplots(1, 2)
+
+    nxi = 1000
+    xis = np.linspace(0, 1, num=nxi)
+
+    K_Gamma = np.zeros((3, nxi))
+    K_Kappa = np.zeros((3, nxi))
+    for i in range(nxi):
+        frame_ID = (xis[i],)
+        elDOF = beam.qDOF_P(frame_ID)
+        qe = q[-1, beam.qDOF][elDOF]
+        _, _, K_Gamma[:, i], K_Kappa[:, i] = beam.eval(qe, xis[i])
+    ax[0].plot(xis, K_Gamma[0], "-r", label="K_Gamma0")
+    ax[0].plot(xis, K_Gamma[1], "-g", label="K_Gamma1")
+    ax[0].plot(xis, K_Gamma[2], "-b", label="K_Gamma2")
+    ax[0].grid()
+    ax[0].legend()
+    ax[1].plot(xis, K_Kappa[0], "-r", label="K_Kappa0")
+    ax[1].plot(xis, K_Kappa[1], "-g", label="K_Kappa1")
+    ax[1].plot(xis, K_Kappa[2], "-b", label="K_Kappa2")
+    ax[1].grid()
+    ax[1].legend()
+
+    ############################
+    # Visualize tip displacement
+    ############################
+    elDOF = beam.qDOF[beam.elDOF[-1]]
+    r_OP = np.array([beam.r_OP(ti, qi[elDOF], (1,)) for (ti, qi) in zip(t, q)])
+
+    fig, ax = plt.subplots()
+
+    ax.plot(t, r_OP[:, 0], "-k", label="x")
+    ax.plot(t, r_OP[:, 1], "--k", label="y")
+    ax.plot(t, r_OP[:, 2], "-.k", label="z")
+    ax.set_xlabel("t")
+    ax.set_ylabel("tip displacement")
+    ax.grid()
+    ax.legend()
+
+    ###########
+    # animation
+    ###########
+    animate_beam(t, q, [beam], L, show=True)
+
+
 def HeavyTop():
     class RigidHeavyTopODE:
         def __init__(self, m, r, l, g):
@@ -1033,19 +1271,7 @@ def BucklingRightHingedFrame(follower=True):
 if __name__ == "__main__":
     # locking_quater_circle()
     # objectivity_quater_circle()
-    convergence_quater_circle()
+    # convergence_quater_circle()
+    HelixIbrahimbegovic1997()
     # HeavyTop()
     # BucklingRightHingedFrame()
-
-    # run(statics=True)
-    # run(statics=False)
-    # locking(case="quater_circle")
-    # locking(case="helix")
-    # SE3_interpolation()
-    # HelixIbrahimbegovic1997()
-    # Dschanibekow()
-    # distributed_force()
-    # objectivity_quater_circle()
-    # convergence_quater_circle()
-    # Noor1981()
-    # Bernoulli()
