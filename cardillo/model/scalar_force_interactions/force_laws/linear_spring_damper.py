63bc5033cd13f0b1a4187277137361915c799d16
Lisa Eberhardt <lisa.eberhardt@inm.uni-stuttgart.de>
2022-09-20 16:58:58 +0200

    comment F_g back in

--- cardillo/model/scalar_force_interactions/force_laws/linear_spring_damper.py
+++ cardillo/model/scalar_force_interactions/force_laws/linear_spring_damper.py
@@ -16,8 +16,8 @@ class Linear_spring_damper:
     def F(self, t, g, gamma):
         return self.d * gamma
 
-    # def F_g(self, t, g, gamma):
-    #     return 0
+    def F_g(self, t, g, gamma):
+        return 0
 
     def F_gamma(self, t, g, gamma):
         return self.d
