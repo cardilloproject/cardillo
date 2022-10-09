8169b90ca4cec9c9262ffc009d1558a7d63f1f2c
Lisa Eberhardt <lisa.eberhardt@inm.uni-stuttgart.de>
2022-10-09 17:42:48 +0200

    mypy config file

--- cardillo/model/bilateral_constraints/implicit/__init__.py
+++ cardillo/model/bilateral_constraints/implicit/__init__.py
@@ -5,7 +5,7 @@ from .RigidConnection import (
     RigidConnectionCable,
 )
 
-# from .revolute_joint import Revolute_joint, Revolute_joint2D
+from .RevoluteJoint import Revolute_joint, Revolute_joint2D
 # from .linear_guidance import Linear_guidance_x, Linear_guidance_x_2D, Linear_guidance_xyz, Linear_guidance_xyz_2D
 from .linear_guidance import Linear_guidance_xyz
 from .RollingCondition import *
