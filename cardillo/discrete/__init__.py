1a8ee8059478f2f5537a59523ce027e4abc1b27e
Jonas Harsch <jonas_harsch@web.de>
2022-10-10 16:17:23 +0200

    Renamed rigid body files.

--- cardillo/discrete/__init__.py
+++ cardillo/discrete/__init__.py
@@ -1,4 +1,4 @@
 from .frame import Frame
-from .RigidBodyEuler import RigidBodyEuler
-from .RigidBodyAxisAngle import RigidBodyAxisAngle
-from .RigidBodyQuaternion import RigidBodyQuaternion
+from .rigid_body_euler import RigidBodyEuler
+from .rigid_body_axis_angle import RigidBodyAxisAngle
+from .rigid_body_quaternion import RigidBodyQuaternion
