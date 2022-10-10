bbe02326ee83b49704d35b560dba3a41ccd3dd37
Jonas Harsch <jonas_harsch@web.de>
2022-10-10 15:26:55 +0200

    Rebased type checking of Lisa.

--- cardillo/model/bilateral_constraints/implicit/__init__.py
+++ cardillo/model/bilateral_constraints/implicit/__init__.py
@@ -6,8 +6,6 @@ from .RigidConnection import (
 )
 from .RevoluteJoint import RevoluteJoint
 
-from .RevoluteJoint import Revolute_joint, Revolute_joint2D
-
 # from .linear_guidance import Linear_guidance_x, Linear_guidance_x_2D, Linear_guidance_xyz, Linear_guidance_xyz_2D
 from .linear_guidance import Linear_guidance_xyz
 from .RollingCondition import *
