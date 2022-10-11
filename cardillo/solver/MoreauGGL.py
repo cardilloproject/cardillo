Fixed error in rolling condition on velocity and acceleration level (using numerical derivatives).

cardillo/solver/MoreauGGL.py
@@ -3296,23 +3296,23 @@ class NonsmoothDecoupled:
             )
         )
 
-        #########################################
-        # solve for consistent initial conditions
-        #########################################
-        from scipy.optimize import fsolve
+        # #########################################
+        # # solve for consistent initial conditions
+        # #########################################
+        # from scipy.optimize import fsolve
 
-        res = fsolve(self.Rx, self.xk.copy(), full_output=1)
-        self.xk = res[0].copy()
+        # res = fsolve(self.Rx, self.xk.copy(), full_output=1)
+        # self.xk = res[0].copy()
 
-        (
-            self.q_dotk,
-            self.u_dotk,
-            self.la_gk,
-            self.la_gammak,
-            self.la_Nk,
-            self.la_Fk,
-        ) = self.unpack_x(self.xk)
-        print(f"la_g0: {self.la_gk}")
+        # (
+        #     self.q_dotk,
+        #     self.u_dotk,
+        #     self.la_gk,
+        #     self.la_gammak,
+        #     self.la_Nk,
+        #     self.la_Fk,
+        # ) = self.unpack_x(self.xk)
+        # print(f"la_g0: {self.la_gk}")
 
         # initialize index sets
         self.I_Nk1 = np.zeros(self.nla_N, dtype=bool)

reference_examples/rolling_disc.py
@@ -18,13 +18,7 @@ from cardillo.solver import (
     EulerBackward,
     GeneralizedAlphaFirstOrder,
     NonsmoothDecoupled,
-    # GeneralizedAlphaFirstOrder,
-    # GeneralizedAlphaSecondOrder,
-    # GenAlphaFirstOrderGGL2_V1,
-    # GenAlphaFirstOrderGGL2_V2,
-    # GenAlphaFirstOrderGGL2_V3,
-    # GeneralizedAlphaFirstOrderGGLGiuseppe,
-    # NonsmoothHalfExplicitEuler,
+    NonsmoothHalfExplicitEuler,
 )
 
 
@@ -134,8 +128,8 @@ def state():
     Lesaux2005: https://doi.org/10.1007/s00332-004-0655-4
     """
     t0 = 0
-    t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.05
-    # t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.25
+    # t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.05
+    t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.25
     # t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.3  # used for GAMM
     # t1 = 2 * np.pi / np.abs(alpha_dot0) * 0.5
     # t1 = 2 * np.pi / np.abs(alpha_dot0) * 1.0
@@ -145,17 +139,20 @@ def state():
     dt = 1.0e-2  # used for GAMM with R = 10 * r
     # dt = 1.0e-3
 
+    # rho_inf = 0.99
     # rho_inf = 0.96  # used for GAMM (high oszillations)
     rho_inf = 0.85  # used for GAMM (low oszillations)
     # rho_inf = 0.1
     # see Arnodl2016, p. 118
     tol = 1.0e-10
 
-    sol = ScipyIVP(model, t1, dt).solve()
+    # sol = ScipyIVP(model, t1, dt).solve()
     # sol = EulerBackward(model, t1, dt).solve()
     # sol = NonsmoothHalfExplicitEuler(model, t1, dt).solve()
-    # sol = GeneralizedAlphaFirstOrder(model, t1, dt, rho_inf=rho_inf, tol=tol).solve()
+    sol = GeneralizedAlphaFirstOrder(model, t1, dt, rho_inf=rho_inf, tol=tol).solve()
+    # sol = GeneralizedAlphaFirstOrder(model, t1, dt, rho_inf=rho_inf, tol=tol, GGL=1).solve()
     # sol = NonsmoothDecoupled(model, t1, dt).solve()
+    # sol = NonsmoothHalfExplicitEuler(model, t1, dt).solve()
 
     t = sol.t
     q = sol.q