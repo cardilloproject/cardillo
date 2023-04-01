109a065d8e9a96c61549bb76707f006bdd5f246a
Jonas Harsch <jonas.harsch@inm.uni-stuttgart.de>
2023-04-01 17:50:48 +0200

    First stepts for least squares fitting for arbitrary initial configurations for rods.

---
+++ cardillo/beams/_fitting.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+# from scipy.optimize import minimize
+from scipy.optimize import least_squares
+from cardillo.math import SE3, SE3inv, Log_SE3
+
+
+def fit_configuration(
+    rod,
+    r_OPs,
+    A_IKs,
+):
+    # number of sample points
+    n_samples = len(r_OPs)
+
+    # linear spaced evaluation points for the rod
+    xis = np.linspace(0, 1, n_samples)
+
+    # # cord length of centerline points
+    # segment_lengths = np.linalg.norm(np.diff(r_OPs, axis=0), axis=1)
+    # cord_lengths = np.cumsum(segment_lengths)
+    # length = cord_lengths[-1]
+    # xis = np.zeros(n_samples, dtype=float)
+    # xis[1:] = cord_lengths / length
+
+    # cord length of relative twists
+    Hs = np.array([SE3(A_IKs[i], r_OPs[i]) for i in range(n_samples)])
+    # relative_twists = np.array(
+    #     [Log_SE3(SE3inv(Hs[i - 1]) @ Hs[i]) for i in range(1, n_samples)]
+    # )
+    # segment_lengths = np.linalg.norm(relative_twists, axis=1)
+    # cord_lengths = np.cumsum(segment_lengths)
+    # xis = np.zeros(n_samples, dtype=float)
+    # xis[1:] = cord_lengths / cord_lengths[-1]
+
+    # initial vector of generalized coordinates
+    Q = np.zeros(rod.nq, dtype=float)
+
+    ###########
+    # warmstart
+    ###########
+    # initialized nodal unknowns with closest data w.r.t. xi values
+    xi_idx_r = np.array(
+        [
+            np.where(xis >= rod.mesh_r.knot_vector.data[i])[0][0]
+            for i in range(rod.mesh_r.nnodes)
+        ]
+    )
+    xi_idx_psi = np.array(
+        [
+            np.where(xis >= rod.mesh_psi.knot_vector.data[i])[0][0]
+            for i in range(rod.mesh_psi.nnodes)
+        ]
+    )
+
+    for node, xi_idx in enumerate(xi_idx_r):
+        Q[rod.nodalDOF_r[node]] = r_OPs[xi_idx]
+    for node, xi_idx in enumerate(xi_idx_psi):
+        Q[rod.nodalDOF_psi[node]] = rod.RotationBase.Log_SO3(A_IKs[xi_idx])
+
+    # TODO: We should always use the axis-angle rotation parametrization for
+    # this fitting since it converges very good. Sadly, this breaks with the
+    # corent way the rod hierarchie is implemented.
+    def residual(q):
+        R = np.zeros(6 * n_samples, dtype=q.dtype)
+        for i, xi in enumerate(xis):
+            # find element number and extract elemt degrees of freedom
+            el = rod.element_number(xi)
+            elDOF = rod.elDOF[el]
+            qe = q[elDOF]
+
+            # interpolate position and orientation
+            r_OP, A_IK, _, _ = rod._eval(qe, xi)
+
+            # compute homogeneous transformation
+            H = SE3(A_IK, r_OP)
+
+            # use relative twist as error measure
+            R[6 * i : 6 * (i + 1)] = Log_SE3(SE3inv(H) @ Hs[i])
+
+        return R
+
+    # def jacobian(q):
+    #     nxis = len(xis)
+    #     nq = len(q)
+    #     J = np.zeros((3 * nxis, nq), dtype=float)
+
+    #     for i, xii in enumerate(xis):
+    #         # find element number and extract elemt degrees of freedom
+    #         el = node_vector.element_number(xii)[0]
+    #         elDOF = mesh.elDOF[el]
+    #         qe = q[elDOF]
+    #         nqe = len(qe)
+
+    #         # evaluate shape functions
+    #         N = mesh.eval_basis(xii)
+
+    #         # interpoalte rotations
+    #         A_IK = np.zeros((3, 3), dtype=float)
+    #         A_IK_qe = np.zeros((3, 3, nqe), dtype=float)
+    #         for node in range(mesh.nnodes_per_element):
+    #             nodalDOF = mesh.nodalDOF_element[node]
+    #             qe_node = qe[nodalDOF]
+    #             A_IK += N[node] * Exp_SO3(qe_node)
+    #             A_IK_qe[:, :, nodalDOF] += N[node] * Exp_SO3_psi(qe_node)
+
+    #         # compute relative rotation vector
+    #         psi_rel_A_rel = Log_SO3_A(r_OPs[i].T @ A_IK)
+    #         psi_rel_qe = np.einsum(
+    #             "ikl,mk,mlj->ij", psi_rel_A_rel, r_OPs[i], A_IK_qe
+    #         )
+
+    #         # insert to residual
+    #         J[3 * i : 3 * (i + 1), elDOF] = psi_rel_qe
+
+    #         # def psi_rel(qe):
+    #         #     # evaluate shape functions
+    #         #     N = mesh.eval_basis(xii)
+
+    #         #     # interpoalte rotations
+    #         #     A_IK = np.zeros((3, 3), dtype=q.dtype)
+    #         #     for node in range(mesh.nnodes_per_element):
+    #         #         A_IK += N[node] * Exp_SO3(qe[mesh.nodalDOF_element[node]])
+
+    #         #     # compute relative rotation vector
+    #         #     return Log_SO3(A_IKs[i].T @ A_IK)
+
+    #         # psi_rel_qe_num = approx_fprime(qe, psi_rel)
+    #         # diff = psi_rel_qe - psi_rel_qe_num
+    #         # error = np.linalg.norm(diff)
+    #         # print(f"error psi_rel_qe: {error}")
+
+    #         # # insert to residual
+    #         # J[3 * i:3 * (i + 1), elDOF] = psi_rel_qe_num
+
+    #     return J
+
+    res = least_squares(
+        residual,
+        Q,
+        # jac="cs",
+        # jac=jacobian,
+        # jac="2-point",
+        # ftol=1e-3,
+        # xtol=1e-3,
+        # gtol=1e-3,
+        # method="lm",
+        method="trf",
+        verbose=2,
+    )
+
+    # from scipy.optimize import leastsq
+    # res = leastsq(residual, q0)
+
+    Q = res.x
+    rod.set_initial_strains(Q)
+
+    print(f"res: {res}")
+    return Q
+    # return res.x
+    # exit()
+
+    # print(f"")
+
+    # def cost_function(q):
+    #     R = residual(q)
+    #     return 0.5 * np.sqrt(R @ R)
+
+    # method = "SLSQP"
+    # res = minimize(
+    #     cost_function,
+    #     q0,
+    #     method=method,
+    #     tol=1.0e-3,
+    #     options={
+    #         "maxiter": 1000,
+    #         "disp": True,
+    #     },
+    # )
+
+    return res.x
---
+++ examples/cruved_initial_configuration_rod.py
@@ -0,0 +1,157 @@
+from cardillo.math import e3
+from cardillo.beams import (
+    RectangularCrossSection,
+    Simo1986,
+)
+from cardillo.constraints import RigidConnection
+
+# from cardillo.beams import K_R12_PetrovGalerkin_Quaternion as Rod
+
+from cardillo.beams import K_R12_PetrovGalerkin_AxisAngle as Rod
+
+# from cardillo.beams import K_SE3_PetrovGalerkin_AxisAngle as Rod
+# from cardillo.beams import K_SE3_PetrovGalerkin_Quaternion as Rod
+# from cardillo.beams import K_SE3_PetrovGalerkin_R9 as Rod
+# from cardillo.beams import K_R3_SO3_PetrovGalerkin_AxisAngle as Rod
+from cardillo.beams import animate_beam
+from cardillo.forces import K_Moment
+from cardillo import System
+from cardillo.solver import Newton
+
+from cardillo.beams._fitting import fit_configuration
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    # number of elements
+    nelements = 5
+
+    # used polynomial degree
+    polynomial_degree = 2
+    basis = "Lagrange"
+
+    # Young's and shear modulus
+    E = 1.0
+    G = 0.5
+
+    # length of the rod
+    L = 1.0e3
+
+    # slenderness and corresponding absolute tolerance for Newton-Raphson solver
+    slenderness = 1.0e2
+    atol = 1.0e-10
+
+    # used cross section
+    width = L / slenderness
+
+    # cross section
+    line_density = 1
+    cross_section = RectangularCrossSection(line_density, width, width)
+    A_rho0 = line_density * cross_section.area
+    K_S_rho0 = line_density * cross_section.first_moment
+    K_I_rho0 = line_density * cross_section.second_moment
+    A = cross_section.area
+
+    # quadratic beam material
+    Ip, I2, I3 = np.diag(cross_section.second_moment)
+    Ei = np.array([E * A, G * A, G * A])
+    Fi = np.array([G * Ip, E * I2, E * I3])
+    material_model = Simo1986(Ei, Fi)
+
+    # left rod
+    Q = Rod.straight_configuration(
+        polynomial_degree,
+        polynomial_degree,
+        basis,
+        basis,
+        nelements,
+        L,
+    )
+    rod = Rod(
+        cross_section,
+        material_model,
+        A_rho0,
+        K_S_rho0,
+        K_I_rho0,
+        polynomial_degree,
+        polynomial_degree,
+        nelements,
+        Q=Q,
+        q0=Q,
+        basis_r=basis,
+        basis_psi=basis,
+    )
+
+    # helix
+    n = 1  # number of helix coils
+    scale = 1.0e1
+    R0 = 1 * scale  # helix radius
+    h = 5 * scale  # helix height
+    c = h / (R0 * 2 * np.pi * n)
+    L = np.sqrt(1 + c**2) * R0 * 2 * np.pi * n
+    print(f"R0: {R0}")
+    print(f"h: {h}")
+    print(f"c: {c}")
+    print(f"n: {n}")
+    print(f"L: {L}")
+
+    # reference solution
+    def r(xi):
+        alpha = 2 * np.pi * n * xi
+        return R0 * np.array([np.sin(alpha), -np.cos(alpha), c * alpha])
+
+    def A_IK(xi):
+        alpha = 2 * np.pi * n * xi
+        sa = np.sin(alpha)
+        ca = np.cos(alpha)
+
+        e_x = np.array([ca, sa, c]) / np.sqrt(1 + c**2)
+        e_y = np.array([-sa, ca, 0])
+        e_z = np.array([-c * ca, -c * sa, 1]) / np.sqrt(1 + c**2)
+
+        return np.vstack((e_x, e_y, e_z)).T
+
+    nxi = 30
+    xis = np.linspace(0, 1, num=nxi)
+
+    r_OPs = np.array([r(xi) for xi in xis])
+    A_IKs = np.array([A_IK(xi) for xi in xis])
+
+    Q = fit_configuration(rod, r_OPs, A_IKs)
+    rod.q0 = Q.copy()
+
+    # joint between origin and left rod
+    system = System()
+    joint1 = RigidConnection(system.origin, rod, frame_ID2=(0,))
+
+    # moment at right beam's tip
+    Fi = material_model.Fi
+    m = Fi[2] * 2 * np.pi / (2 * L) * 0.25 * 0.1
+    M = lambda t: t * e3 * m
+    moment = K_Moment(M, rod, (1,))
+
+    # assemble the system
+    system.add(rod, joint1)
+    # system.add(moment)
+    system.assemble()
+
+    # solve static system
+    n_load_steps = 3
+    solver = Newton(
+        system,
+        n_load_steps=n_load_steps,
+        atol=atol,
+    )
+    sol = solver.solve()
+    q = sol.q
+    nt = len(q)
+    t = sol.t[:nt]
+
+    # t = [0, 1]
+    # q = [q0, q0]
+
+    ###########
+    # animation
+    ###########
+    animate_beam(t, q, [rod], 2 * scale, show=True)
