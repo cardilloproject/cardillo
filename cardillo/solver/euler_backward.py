59fc9892f6d7775e40d76c241f49f15b0058fb2c
Jonas Harsch <jonas.harsch@inm.uni-stuttgart.de>
2023-04-06 14:49:43 +0200

    Added constrained stabilization for quaternion length.

--- cardillo/solver/euler_backward.py
+++ cardillo/solver/euler_backward.py
@@ -329,8 +329,15 @@ class NonsmoothBackwardEuler:
         self.nla_gamma = system.nla_gamma
         self.nla_N = system.nla_N
         self.nla_F = system.nla_F
+        self.nla_S = self.system.nla_S
         self.ny = (
-            self.nq + self.nu + self.nla_g + self.nla_gamma + self.nla_N + self.nla_F
+            self.nq
+            + self.nu
+            + self.nla_g
+            + self.nla_gamma
+            + self.nla_N
+            + self.nla_F
+            + self.nla_S
         )
         self.split = np.cumsum(
             np.array(
@@ -340,6 +347,7 @@ class NonsmoothBackwardEuler:
                     self.nla_g,
                     self.nla_gamma,
                     self.nla_N,
+                    self.nla_F,
                 ],
                 dtype=int,
             )
@@ -372,6 +380,7 @@ class NonsmoothBackwardEuler:
                 self.la_gamman,
                 self.la_Nn,
                 self.la_Fn,
+                np.zeros(self.nla_S),
             )
         )
 
@@ -381,7 +390,7 @@ class NonsmoothBackwardEuler:
     def R(self, yn1, update_index=False):
         tn, dt, qn, un = self.tn, self.dt, self.qn, self.un
 
-        q_dotn1, u_dotn1, la_gn1, la_gamman1, la_Nn1, la_Fn1 = np.array_split(
+        q_dotn1, u_dotn1, la_gn1, la_gamman1, la_Nn1, la_Fn1, mu_Sn1 = np.array_split(
             yn1, self.split
         )
         tn1 = tn + dt
@@ -396,7 +405,10 @@ class NonsmoothBackwardEuler:
         ####################
         # kinematic equation
         ####################
-        R[: self.split[0]] = q_dotn1 - self.system.q_dot(tn1, qn1, un1)
+        g_S_q = self.system.g_S_q(tn1, qn1, scipy_matrix=csc_matrix)
+        R[: self.split[0]] = (
+            q_dotn1 - self.system.q_dot(tn1, qn1, un1) - g_S_q.T @ mu_Sn1
+        )
 
         ####################
         # euations of motion
@@ -429,21 +441,6 @@ class NonsmoothBackwardEuler:
         ##########
         # friction
         ##########
-        # gamma_Fn1 = self.system.gamma_F(tn1, qn1, un1)
-        # mu = self.system.mu
-        # prox_r_F = self.prox_r_F
-        # for i_N, i_F in enumerate(self.system.NF_connectivity):
-        #     i_F = np.array(i_F)
-
-        #     if len(i_F) > 0:
-        #         # Note: This is the simplest formulation for friction but we
-        #         # subsequently decompose the function it into both cases of
-        #         # the prox_sphere function for easy derivation
-        #         R[self.split[4] + i_F] = la_Fn1[i_F] + prox_sphere(
-        #             prox_r_F[i_N] * gamma_Fn1[i_F] - la_Fn1[i_F],
-        #             mu[i_N] * la_Nn1[i_N],
-        #         )
-
         prox_r_F = self.prox_r_F
         mu = self.system.mu
         gamma_F = self.system.gamma_F(tn1, qn1, un1)
@@ -464,18 +461,18 @@ class NonsmoothBackwardEuler:
                     R[self.split[4] + i_F] = gamma_Fi
                 else:
                     if norm_arg_F > 0:
-                        R[self.split[4] + i_F] = (
-                            la_Fi + radius * arg_F / norm_arg_F
-                        )
+                        R[self.split[4] + i_F] = la_Fi + radius * arg_F / norm_arg_F
                     else:
                         R[self.split[4] + i_F] = la_Fi + radius * arg_F
+
+        R[self.split[5] :] = self.system.g_S(tn1, qn1)
 
         return R
 
     def J(self, yn1, *args, **kwargs):
         tn, dt, qn, un = self.tn, self.dt, self.qn, self.un
 
-        q_dotn1, u_dotn1, la_gn1, la_gamman1, la_Nn1, la_Fn1 = np.array_split(
+        q_dotn1, u_dotn1, la_gn1, la_gamman1, la_Nn1, la_Fn1, mu_Sn1 = np.array_split(
             yn1, self.split
         )
         tn1 = tn + dt
@@ -485,8 +482,13 @@ class NonsmoothBackwardEuler:
         ####################
         # kinematic equation
         ####################
-        Rq_q = eye(self.nq) - dt * self.system.q_dot_q(tn1, qn1, un1)
+        Rq_q = eye(self.nq) - dt * (
+            self.system.q_dot_q(tn1, qn1, un1)
+            + self.system.g_S_q_T_mu_q(tn1, qn1, mu_Sn1)
+        )
         Rq_u = -dt * self.system.B(tn1, qn1)
+        # Rq_mu_S = -self.system.g_S_q(tn1, qn1).T
+        g_S_q = self.system.g_S_q(tn1, qn1)
 
         ########################
         # euations of motion (1)
@@ -589,18 +591,19 @@ class NonsmoothBackwardEuler:
         # fmt: off
         J = bmat(
             [
-                [Rq_q, Rq_u, None, None, None, None],
-                [Ru_q, Ru_u, -W_g, -W_gamma, -W_N, -W_F],
-                [Rla_g_q, None, None, None, None, None],
-                [Rla_gamma_q, Rla_gamma_u, None, None, None, None],
-                [Rla_N_q, None, None, None, Rla_N_la_N, None],
-                [dt * Rla_F_q, dt * Rla_F_u, None, None, Rla_F_la_N, Rla_F_la_F],
+                [Rq_q, Rq_u, None, None, None, None, -g_S_q.T],
+                [Ru_q, Ru_u, -W_g, -W_gamma, -W_N, -W_F, None],
+                [Rla_g_q, None, None, None, None, None, None],
+                [Rla_gamma_q, Rla_gamma_u, None, None, None, None, None],
+                [Rla_N_q, None, None, None, Rla_N_la_N, None, None],
+                [dt * Rla_F_q, dt * Rla_F_u, None, None, Rla_F_la_N, Rla_F_la_F, None],
+                [dt * g_S_q, None, None, None, None, None, None],
             ],
             format="csr",
         )
         # fmt: on
 
-        # return J
+        return J
 
         # J_num = csr_matrix(approx_fprime(yn1, self.R))
         J_num = csr_matrix(approx_fprime(yn1, self.R, method="2-point", eps=1e-6))
@@ -609,14 +612,15 @@ class NonsmoothBackwardEuler:
         diff = (J - J_num).toarray()
         # diff = diff[:self.split[0]]
         # diff = diff[self.split[0] : self.split[1]]
-        diff = diff[self.split[0] : self.split[1], : self.split[0]] # TODO: spot error
+        # diff = diff[self.split[0] : self.split[1], : self.split[0]]  # TODO: spot error
         # diff = diff[self.split[0] : self.split[1], self.split[0] :]
         # diff = diff[self.split[1]:self.split[2]]
         # diff = diff[self.split[2]:self.split[3]]
         # diff = diff[self.split[3]:self.split[4]]
-        # diff = diff[self.split[4] :]
-        # diff = diff[self.split[4] :, : self.split[0]]
-        # diff = diff[self.split[4] :, self.split[0] : self.split[1]]
+        # diff = diff[self.split[4] : self.split[5]]
+        # diff = diff[self.split[4] : self.split[5], : self.split[0]]
+        # diff = diff[self.split[4] : self.split[5], self.split[0] : self.split[1]]
+        # diff = diff[self.split[5] :]
         error = np.linalg.norm(diff)
         if error > 1.0e-8:
             print(f"error J: {error}")
@@ -671,9 +675,15 @@ class NonsmoothBackwardEuler:
 
                 break
 
-            q_dotn1, u_dotn1, la_gn1, la_gamman1, la_Nn1, la_Fn1 = np.array_split(
-                yn1, self.split
-            )
+            (
+                q_dotn1,
+                u_dotn1,
+                la_gn1,
+                la_gamman1,
+                la_Nn1,
+                la_Fn1,
+                mu_Sn1,
+            ) = np.array_split(yn1, self.split)
             qn1 = self.qn + self.dt * q_dotn1
             un1 = self.un + self.dt * u_dotn1
 
