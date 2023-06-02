0e2dfc4e8291f9847f4eb03e815f0f13918a0fe5
Lisa Eberhardt <lisa.eberhardt@inm.uni-stuttgart.de>
2023-06-02 08:09:29 +0200

    rod refactoring and correctWla_g_q

--- cardillo/constraints/rod.py
+++ cardillo/constraints/rod.py
@@ -9,19 +9,19 @@ class Rod:
         subsystem2,
         frame_ID1=np.zeros(3),
         frame_ID2=np.zeros(3),
-        K_r_SP1=np.zeros(3),
-        K_r_SP2=np.zeros(3),
+        K1_r_SP1=np.zeros(3),
+        K2_r_SP2=np.zeros(3),
         la_g0=None,
     ):
         self.nla_g = 1
         self.la_g0 = np.zeros(self.nla_g) if la_g0 is None else la_g0
         self.subsystem1 = subsystem1
         self.frame_ID1 = frame_ID1
-        self.K_r_SP1 = K_r_SP1
+        self.K1_r_SP1 = K1_r_SP1
 
         self.subsystem2 = subsystem2
         self.frame_ID2 = frame_ID2
-        self.K_r_SP2 = K_r_SP2
+        self.K2_r_SP2 = K2_r_SP2
 
     def assembler_callback(self):
         qDOF1 = self.subsystem1.qDOF
@@ -43,72 +43,72 @@ class Rod:
         self.__nu = self.__nu1 + self.__nu2
 
         self.r_OP1 = lambda t, q: self.subsystem1.r_OP(
-            t, q[:nq1], self.frame_ID1, self.K_r_SP1
+            t, q[:nq1], self.frame_ID1, self.K1_r_SP1
         )
-        self.r_OP1_q = lambda t, q: self.subsystem1.r_OP_q(
-            t, q[:nq1], self.frame_ID1, self.K_r_SP1
+        self.r_OP1_q1 = lambda t, q: self.subsystem1.r_OP_q(
+            t, q[:nq1], self.frame_ID1, self.K1_r_SP1
         )
         self.v_P1 = lambda t, q, u: self.subsystem1.v_P(
-            t, q[:nq1], u[:nu1], self.frame_ID1, self.K_r_SP1
+            t, q[:nq1], u[:nu1], self.frame_ID1, self.K1_r_SP1
         )
-        self.v_P1_q = lambda t, q, u: self.subsystem1.v_P_q(
-            t, q[:nq1], u[:nu1], self.frame_ID1, self.K_r_SP1
+        self.v_P1_q1 = lambda t, q, u: self.subsystem1.v_P_q(
+            t, q[:nq1], u[:nu1], self.frame_ID1, self.K1_r_SP1
         )
         self.a_P1 = lambda t, q, u, u_dot: self.subsystem1.a_P(
-            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K_r_SP1
+            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K1_r_SP1
         )
-        self.a_P1_q = lambda t, q, u, u_dot: self.subsystem1.a_P_q(
-            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K_r_SP1
+        self.a_P1_q1 = lambda t, q, u, u_dot: self.subsystem1.a_P_q(
+            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K1_r_SP1
         )
-        self.a_P1_u = lambda t, q, u, u_dot: self.subsystem1.a_P_u(
-            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K_r_SP1
+        self.a_P1_u1 = lambda t, q, u, u_dot: self.subsystem1.a_P_u(
+            t, q[:nq1], u[:nu1], u_dot[:nu1], self.frame_ID1, self.K1_r_SP1
         )
         self.J_P1 = lambda t, q: self.subsystem1.J_P(
-            t, q[:nq1], self.frame_ID1, self.K_r_SP1
+            t, q[:nq1], self.frame_ID1, self.K1_r_SP1
         )
-        self.J_P1_q = lambda t, q: self.subsystem1.J_P_q(
-            t, q[:nq1], self.frame_ID1, self.K_r_SP1
+        self.J_P1_q1 = lambda t, q: self.subsystem1.J_P_q(
+            t, q[:nq1], self.frame_ID1, self.K1_r_SP1
         )
 
         self.r_OP2 = lambda t, q: self.subsystem2.r_OP(
-            t, q[nq1:], self.frame_ID2, self.K_r_SP2
+            t, q[nq1:], self.frame_ID2, self.K2_r_SP2
         )
-        self.r_OP2_q = lambda t, q: self.subsystem2.r_OP_q(
-            t, q[nq1:], self.frame_ID2, self.K_r_SP2
+        self.r_OP2_q2 = lambda t, q: self.subsystem2.r_OP_q(
+            t, q[nq1:], self.frame_ID2, self.K2_r_SP2
         )
         self.v_P2 = lambda t, q, u: self.subsystem2.v_P(
-            t, q[nq1:], u[nu1:], self.frame_ID2, self.K_r_SP2
+            t, q[nq1:], u[nu1:], self.frame_ID2, self.K2_r_SP2
         )
-        self.v_P2_q = lambda t, q, u: self.subsystem2.v_P_q(
-            t, q[nq1:], u[nu1:], self.frame_ID2, self.K_r_SP2
+        self.v_P2_q2 = lambda t, q, u: self.subsystem2.v_P_q(
+            t, q[nq1:], u[nu1:], self.frame_ID2, self.K2_r_SP2
         )
         self.a_P2 = lambda t, q, u, u_dot: self.subsystem2.a_P(
-            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K_r_SP2
+            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K2_r_SP2
         )
-        self.a_P2_q = lambda t, q, u, u_dot: self.subsystem2.a_P_q(
-            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K_r_SP2
+        self.a_P2_q2 = lambda t, q, u, u_dot: self.subsystem2.a_P_q(
+            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K2_r_SP2
         )
-        self.a_P2_u = lambda t, q, u, u_dot: self.subsystem2.a_P_u(
-            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K_r_SP2
+        self.a_P2_u2 = lambda t, q, u, u_dot: self.subsystem2.a_P_u(
+            t, q[nq1:], u[nu1:], u_dot[nu1:], self.frame_ID2, self.K2_r_SP2
         )
         self.J_P2 = lambda t, q: self.subsystem2.J_P(
-            t, q[nq1:], self.frame_ID2, self.K_r_SP2
+            t, q[nq1:], self.frame_ID2, self.K2_r_SP2
         )
-        self.J_P2_q = lambda t, q: self.subsystem2.J_P_q(
-            t, q[nq1:], self.frame_ID2, self.K_r_SP2
+        self.J_P2_q2 = lambda t, q: self.subsystem2.J_P_q(
+            t, q[nq1:], self.frame_ID2, self.K2_r_SP2
         )
 
         r_OP10 = self.subsystem1.r_OP(
             self.subsystem1.t0,
             self.subsystem1.q0[local_qDOF1],
             self.frame_ID1,
-            self.K_r_SP1,
+            self.K1_r_SP1,
         )
         r_OP20 = self.subsystem2.r_OP(
             self.subsystem2.t0,
             self.subsystem2.q0[local_qDOF2],
             self.frame_ID2,
-            self.K_r_SP2,
+            self.K2_r_SP2,
         )
         self.dist = np.linalg.norm(r_OP20 - r_OP10)
         if self.dist < 1e-6:
@@ -122,8 +122,8 @@ class Rod:
     def g_q(self, t, q):
         r_OP1 = self.r_OP1(t, q)
         r_OP2 = self.r_OP2(t, q)
-        r_OP1_q = self.r_OP1_q(t, q)
-        r_OP2_q = self.r_OP2_q(t, q)
+        r_OP1_q = self.r_OP1_q1(t, q)
+        r_OP2_q = self.r_OP2_q2(t, q)
         return np.array(
             [2 * (r_OP2 - r_OP1) @ np.hstack([-r_OP1_q, r_OP2_q])], dtype=q.dtype
         )
@@ -139,10 +139,10 @@ class Rod:
 
         g_dot_q = np.zeros((self.nla_g, self.__nq))
         g_dot_q[:, : self.__nq1] = -2 * (
-            r_P1P2 @ self.v_P1_q(t, q, u) + v_P1P2 @ self.r_OP1_q(t, q)
+            r_P1P2 @ self.v_P1_q1(t, q, u) + v_P1P2 @ self.r_OP1_q1(t, q)
         )
         g_dot_q[:, self.__nq1 :] = 2 * (
-            r_P1P2 @ self.v_P2_q(t, q, u) + v_P1P2 @ self.r_OP2_q(t, q)
+            r_P1P2 @ self.v_P2_q2(t, q, u) + v_P1P2 @ self.r_OP2_q2(t, q)
         )
 
         return g_dot_q
@@ -164,14 +164,14 @@ class Rod:
 
         g_ddot_q = np.zeros((self.nla_g, self.__nq))
         g_ddot_q[:, : self.__nq1] = -2 * (
-            2 * v_P1P2 @ self.v_P1_q(t, q, u)
-            + a_P1P2 @ self.r_OP1_q(t, q)
-            + r_P1P2 @ self.a_P1_q(t, q, u, u_dot)
+            2 * v_P1P2 @ self.v_P1_q1(t, q, u)
+            + a_P1P2 @ self.r_OP1_q1(t, q)
+            + r_P1P2 @ self.a_P1_q1(t, q, u, u_dot)
         )
         g_ddot_q[:, self.__nq1 :] = 2 * (
-            2 * v_P1P2 @ self.v_P2_q(t, q, u)
-            + a_P1P2 @ self.r_OP2_q(t, q)
-            + r_P1P2 @ self.a_P2_q(t, q, u, u_dot)
+            2 * v_P1P2 @ self.v_P2_q2(t, q, u)
+            + a_P1P2 @ self.r_OP2_q2(t, q)
+            + r_P1P2 @ self.a_P2_q2(t, q, u, u_dot)
         )
 
         return g_ddot_q
@@ -182,10 +182,10 @@ class Rod:
 
         g_ddot_u = np.zeros((self.nla_g, self.__nu))
         g_ddot_u[:, : self.__nu1] = -2 * (
-            2 * v_P1P2 @ self.J_P1(t, q) + r_P1P2 @ self.a_P1_u(t, q, u, u_dot)
+            2 * v_P1P2 @ self.J_P1(t, q) + r_P1P2 @ self.a_P1_u1(t, q, u, u_dot)
         )
         g_ddot_u[:, self.__nu1 :] = 2 * (
-            2 * v_P1P2 @ self.J_P2(t, q) + r_P1P2 @ self.a_P2_u(t, q, u, u_dot)
+            2 * v_P1P2 @ self.J_P2(t, q) + r_P1P2 @ self.a_P2_u2(t, q, u, u_dot)
         )
 
         return g_ddot_u
@@ -208,19 +208,26 @@ class Rod:
         nq1 = self.__nq1
         nu1 = self.__nu1
         r_P1P2 = self.r_OP2(t, q) - self.r_OP1(t, q)
-        r_OP1_q = self.r_OP1_q(t, q)
-        r_OP2_q = self.r_OP2_q(t, q)
+        r_OP1_q1 = self.r_OP1_q1(t, q)
+        r_OP2_q2 = self.r_OP2_q2(t, q)
         J_P1 = self.J_P1(t, q)
         J_P2 = self.J_P2(t, q)
-        J_P1_q = self.J_P1_q(t, q)
-        J_P2_q = self.J_P2_q(t, q)
+        J_P1_q1 = self.J_P1_q1(t, q)
+        J_P2_q2 = self.J_P2_q2(t, q)
 
         # dense blocks
         Wla_g_q = np.zeros((self.__nu, self.__nq))
-        Wla_g_q[:nu1, :nq1] = J_P1.T @ r_OP1_q + np.einsum("i,ijk->jk", -r_P1P2, J_P1_q)
-        Wla_g_q[:nu1, nq1:] = -J_P1.T @ r_OP2_q
-        Wla_g_q[nu1:, :nq1] = -J_P2.T @ r_OP1_q
-        Wla_g_q[nu1:, nq1:] = J_P2.T @ r_OP2_q - np.einsum("i,ijk->jk", r_P1P2, J_P2_q)
+        Wla_g_q[:nu1, :nq1] = (
+            2 * la_g * (J_P1.T @ r_OP1_q1 - np.einsum("i,ijk->jk", r_P1P2, J_P1_q1))
+        )
+        Wla_g_q[:nu1, nq1:] = -2 * la_g * J_P1.T @ r_OP2_q2
+        Wla_g_q[nu1:, :nq1] = -2 * la_g * J_P2.T @ r_OP1_q1
+        Wla_g_q[nu1:, nq1:] = (
+            2 * la_g * (J_P2.T @ r_OP2_q2 + np.einsum("i,ijk->jk", r_P1P2, J_P2_q2))
+        )
+
+        # Wla_g_q_num = approx_fprime(q, lambda q: self.W_g(t,q)*la_g)
+        # error = np.linalg.norm(Wla_g_q-Wla_g_q_num)
 
         return Wla_g_q
 
