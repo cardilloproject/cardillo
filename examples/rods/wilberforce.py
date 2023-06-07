cd294e714f358eac600df11480f5248388351860
Jonas Harsch <jonas.harsch@inm.uni-stuttgart.de>
2023-06-07 12:55:34 +0200

    Reduced spring stiffnesses and removed rod gravity.

--- examples/rods/wilberforce.py
+++ examples/rods/wilberforce.py
@@ -194,6 +194,11 @@ if __name__ == "__main__":
     E = 206e9  # Pa
     G = 81.5e9  # Pa
 
+    # TODO:
+    scale = 1e-2
+    E *= scale
+    G *= scale
+
     # 1mm cross sectional diameter
     d = 1e-3
     r = wire_radius = d / 2
@@ -218,11 +223,11 @@ if __name__ == "__main__":
     ###########################
     # nelements, nturns = 4, 1
     # nelements, nturns = 8, 2
-    # nelements, nturns = 16, 2
+    nelements, nturns = 16, 2
     # nelements, nturns = 16, 4
     # nelements, nturns = 32, 4
     # nelements, nturns = 32, 8
-    nelements, nturns = 64, 8
+    # nelements, nturns = 64, 8
     # nelements, nturns = 64, 16
     # nelements, nturns = 80, 20
 
@@ -361,7 +366,8 @@ if __name__ == "__main__":
     #####################
     # assemble the system
     #####################
-    system.add(rod, joint1, force_rod)
+    # system.add(rod, joint1, force_rod)
+    system.add(rod, joint1)
     system.add(bob, joint2, force_bob)
     system.assemble()
 
@@ -375,16 +381,19 @@ if __name__ == "__main__":
             n_load_steps=n_load_steps,
         )
     else:
-        t1 = 3
+        t1 = 30
+        # t1 = 10
+        # t1 = 3
         # t1 = 1
         # t1 = 0.5
         # dt = 1e-1
-        # dt = 1e-2
+        dt = 1e-2
         # dt = 5e-3
-        dt = 1e-3  # 16 turns
+        # dt = 1e-3  # 16 turns
         # dt = 5e-4
 
         solver = EulerBackward(system, t1, dt, method="index 3")
+        # solver = ScipyIVP(system, t1, dt)
         # solver = ScipyIVP(system, t1, dt, method="RK23", rtol=1e-3, atol=1e-3)
         # solver = RadauIIa(system, t1, dt, dae_index=2, max_step=dt)
         # solver = SimplifiedNonsmoothGeneralizedAlpha(system, t1, dt, rho_inf=0.8)
@@ -428,7 +437,7 @@ if __name__ == "__main__":
     ###########
     # animation
     ###########
-    animate_beam(t, q, [rod], 0.05, scale_di=0.01, show=False)
+    _ = animate_beam(t, q, [rod], 0.05, scale_di=0.01, show=False)
 
     ############
     # VTK export
@@ -439,185 +448,3 @@ if __name__ == "__main__":
     e.export_contr(bob)
 
     plt.show()
-
-    exit()
-
-    # evaluate helixa at discrete points
-    xi = np.linspace(0, turns, nxi)
-    P, dP, ddP = helix3D(xi, coil_radius, pitch_unloaded)
-
-    # compute directors using Serret-Frenet frame
-    d1 = (dP.T / np.linalg.norm(dP, axis=-1)).T
-    d2 = (ddP.T / np.linalg.norm(ddP, axis=-1)).T
-    d3 = np.cross(d1, d2)
-
-    exit()
-    #############################
-    # helix example of Harsch2023
-    #############################
-
-    # Young's and shear modulus
-    E = 1.0
-    G = 0.5
-
-    # length od the rod
-    L = 1.0e3
-
-    # used cross section
-    width = L / slenderness
-
-    # cross section and quadratic beam material
-    line_density = 1
-    # cross_section = RectangularCrossSection(line_density, width, width)
-    cross_section = CircularCrossSection(line_density, width)
-    A_rho0 = line_density * cross_section.area
-    K_S_rho0 = line_density * cross_section.first_moment
-    K_I_rho0 = line_density * cross_section.second_moment
-    A = cross_section.area
-    Ip, I2, I3 = np.diag(cross_section.second_moment)
-    Ei = np.array([E * A, G * A, G * A])
-    Fi = np.array([G * Ip, E * I2, E * I3])
-    material_model = Simo1986(Ei, Fi)
-
-    # starting point and orientation of initial point, initial length
-    r_OP0 = np.zeros(3, dtype=float)
-    A_IK0 = np.eye(3, dtype=float)
-
-    if Rod in [
-        K_SE3_PetrovGalerkin_AxisAngle,
-        K_SE3_PetrovGalerkin_Quaternion,
-        K_SE3_PetrovGalerkin_R9,
-    ]:
-        q0 = Rod.straight_configuration(
-            nelements,
-            L,
-            r_OP=r_OP0,
-            A_IK=A_IK0,
-        )
-        rod = Rod(
-            cross_section,
-            material_model,
-            A_rho0,
-            K_S_rho0,
-            K_I_rho0,
-            nelements,
-            q0,
-        )
-    elif Rod in [
-        K_R12_PetrovGalerkin_AxisAngle,
-        K_R12_PetrovGalerkin_Quaternion,
-        K_R12_PetrovGalerkin_R9,
-    ]:
-        q0 = Rod.straight_configuration(
-            polynomial_degree,
-            polynomial_degree,
-            basis,
-            basis,
-            nelements,
-            L,
-            r_OP=r_OP0,
-            A_IK=A_IK0,
-        )
-        rod = Rod(
-            cross_section,
-            material_model,
-            A_rho0,
-            K_S_rho0,
-            K_I_rho0,
-            polynomial_degree,
-            polynomial_degree,
-            nelements,
-            Q=q0,
-            q0=q0,
-            basis_r=basis,
-            basis_psi=basis,
-        )
-    else:
-        raise NotImplementedError
-
-    frame1 = Frame(r_OP0, A_IK0)
-
-    # left and right joint
-    joint1 = RigidConnection(frame1, rod, frame_ID2=(0,))
-
-    # moment at the beam's tip
-    Fi = material_model.Fi
-    m = Fi[2] * 2 * np.pi / L * 0.25
-    M = lambda t: t * e3 * m
-    moment = K_Moment(M, rod, (1,))
-
-    # force at the beam's tip
-    f_g_rod = m / L
-    F = lambda t: t * f_g_rod * e3
-    print(f"f_max: {F(1)}")
-    force_bob = Force(F, rod, frame_ID=(1,))
-
-    # # moment at right end
-    # Fi = material_model.Fi
-    # # M = lambda t: (e3 * 2 * np.pi * Fi[2] / L * t) * 2
-    # M = lambda t: 2 * np.pi / L * (e1 * Fi[0] + e3 * Fi[2]) * t * 2
-    # # if statics:
-    # #     M = lambda t: (e1 * Fi[0] + e3 * Fi[2]) * 1.0 * t * 2 * np.pi / L * 0.5
-    # # else:
-    # #     M = lambda t: (e1 * Fi[0] + e3 * Fi[2]) * 1.0 * 2 * np.pi / L * 0.05
-    # moment = K_Moment(M, beam, (1,))
-
-    # # force at the rght end
-    # f = lambda t: t * e1 * 1.0e3
-    # force = K_Force(f, beam, (1,))
-
-    # # line distributed body force
-    # if statics:
-    #     l = lambda t, xi: t * (0.5 * e2 - e3) * 5e1
-    # else:
-    #     l = lambda t, xi: (0.5 * e2 - e3) * 5e0
-    # line_force = DistributedForce1DBeam(l, beam)
-
-    # assemble the system
-    system = System()
-    system.add(rod)
-    system.add(frame1)
-    system.add(joint1)
-    system.add(moment)
-    system.add(force_bob)
-    system.assemble()
-
-    if statics:
-        n_load_steps = 10
-        solver = Newton(
-            system,
-            n_load_steps=n_load_steps,
-            max_iter=30,
-            atol=atol,
-        )
-        # solver = Riks(
-        #     system,
-        #     atol=1e-8,
-        # )
-    else:
-        t1 = 1
-        dt = 2.5e-2
-        solver = EulerBackward(system, t1, dt, method="index 1")
-        # solver = EulerBackward(system, t1, dt, method="index 2")
-        # solver = EulerBackward(system, t1, dt, method="index 3")
-        # solver = EulerBackward(system, t1, dt, method="index 2 GGL")
-        # solver = ScipyIVP(system, t1, dt, rtol=1.0e-2, atol=1.0e-2)
-        # solver = ScipyIVP(system, t1, dt, rtol=1.0e-2, atol=1.0e-2, method="Radau")
-
-    sol = solver.solve()
-    q = sol.q
-    nt = len(q)
-    t = sol.t[:nt]
-
-    # ###########
-    # # animation
-    # ###########
-    # animate_beam(t, q, [rod], L, show=True)
-
-    ############
-    # VTK export
-    ############
-    path = Path(__file__)
-    e = Export(path.parent, path.stem, True, 30, sol)
-    e.export_contr(rod, level="centerline + directors", num=20)
-    e.export_contr(rod, level="volume", n_segments=5, num=50)
