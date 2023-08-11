Fixed quaternion interpolation rod.

cardillo/beams/quaternion_interpolation_petrov_galerkin.py
@@ -261,17 +261,19 @@ class K_PetrovGalerkinQuaternionInterpolation(K_TimoshenkoPetrovGalerkinBaseQuat
         )
 
     def A_IK(self, t, q, frame_ID):
-        # evaluate shape functions
-        N_psi, _ = self.basis_functions_psi(frame_ID[0])
+        return self._eval(q, frame_ID[0])[1]
+        # # evaluate shape functions
+        # N_psi, _ = self.basis_functions_psi(frame_ID[0])
 
-        # interpolate orientation
-        A_IK = np.zeros((3, 3), dtype=q.dtype)
-        for node in range(self.nnodes_element_psi):
-            A_IK += N_psi[node] * self.RotationBase.Exp_SO3(
-                q[self.nodalDOF_element_psi[node]]
-            )
+        # # interpolate orientation
+        # A_IK = np.zeros((3, 3), dtype=q.dtype)
+        # for node in range(self.nnodes_element_psi):
+        #     A_IK += N_psi[node] * self.RotationBase.Exp_SO3(
+        #         q[self.nodalDOF_element_psi[node]]
+        #     )
 
-        return A_IK
+        # return A_IK
 
     def A_IK_q(self, t, q, frame_ID):
-        return approx_fprime(q, lambda q: self.A_IK(t, q, frame_ID))
+        # return approx_fprime(q, lambda q: self.A_IK(t, q, frame_ID))
+        return self._deval(q, frame_ID[0])[5]

examples/rods/cantilever_beam.py
@@ -48,8 +48,8 @@ from pathlib import Path
 # Rod = K_SE3_PetrovGalerkin_Quaternion
 # Rod = K_SE3_PetrovGalerkin_R9
 
-# Rod = K_PetrovGalerkinQuaternionInterpolation
-Rod = HigherOrder_K_SE3_PetrovGalerkin_Quaternion
+Rod = K_PetrovGalerkinQuaternionInterpolation
+# Rod = HigherOrder_K_SE3_PetrovGalerkin_Quaternion
 
 statics = True
 # statics = False
@@ -64,12 +64,13 @@ atol = 1.0e-8
 # atol = 1.0e-14
 
 # number of elements
-# nelements = 10
-nelements = 3
+# nelements = 20
+nelements = 5
 
 # used polynomial degree
 # polynomial_degree = 1
-polynomial_degree = 2
+# polynomial_degree = 2
+polynomial_degree = 3
 basis = "Lagrange"
 
 
@@ -202,22 +203,22 @@ if __name__ == "__main__":
     # left and right joint
     joint1 = RigidConnection(frame1, rod, frame_ID2=(0,))
 
-    # # moment at the beam's tip
-    # Fi = material_model.Fi
-    # m = Fi[2] * 2 * np.pi / L * 0.25
-    # M = lambda t: t * e3 * m
-    # moment = K_Moment(M, rod, (1,))
-
-    # # force at the beam's tip
-    # f = m / L
-    # F = lambda t: t * f * e3
-    # print(f"f_max: {F(1)}")
-    # force = Force(F, rod, frame_ID=(1,))
-
-    # moment at right end
+    # moment at the beam's tip
     Fi = material_model.Fi
-    M = lambda t: 2 * np.pi / L * (e1 * Fi[0] + e3 * Fi[2]) * t * 1
+    m = Fi[2] * 2 * np.pi / L * 1.5
+    M = lambda t: t * e3 * m
     moment = K_Moment(M, rod, (1,))
+
+    # force at the beam's tip
+    f = m / L * 10e-1
+    F = lambda t: t * f * e3
+    print(f"f_max: {F(1)}")
+    force = Force(F, rod, frame_ID=(1,))
+
+    # # moment at right end
+    # Fi = material_model.Fi
+    # M = lambda t: 2 * np.pi / L * (e1 * Fi[0] + e3 * Fi[2]) * t * 1
+    # moment = K_Moment(M, rod, (1,))
 
     # # force at the rght end
     # f = lambda t: t * e1 * 1.0e3
@@ -236,11 +237,11 @@ if __name__ == "__main__":
     system.add(frame1)
     system.add(joint1)
     system.add(moment)
-    # system.add(force)
+    system.add(force)
     system.assemble()
 
     if statics:
-        n_load_steps = 20
+        n_load_steps = 40
         solver = Newton(
             system,
             n_load_steps=n_load_steps,