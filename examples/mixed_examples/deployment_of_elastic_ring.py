4bcd8bb627d66228ed9fe76ad73ac5498d8e92f3
Domenico Castello <domenicocastello.950@gmail.com>
2023-10-19 11:13:42 +0200

    ring

---
+++ examples/mixed_examples/deployment_of_elastic_ring.py
@@ -0,0 +1,181 @@
+from cardillo.beams import (
+    CosseratRodPG_SE3,
+    CosseratRodPG_Quat,
+    CosseratRodPG_R12,
+    RectangularCrossSection,
+    CircularCrossSection,
+    Simo1986,
+    animate_beam,
+)
+
+
+from cardillo.beams.cosseratRodPGMixed import (
+    CosseratRodPG_R12Mixed,
+    CosseratRodPG_QuatMixed,
+    CosseratRodPG_SE3Mixed,
+)
+
+from cardillo.discrete import Frame
+from cardillo.constraints import RigidConnection, Cylindrical
+from cardillo.solver import Newton
+from cardillo.forces import Force, Moment, K_Force, K_Moment
+
+from cardillo.math import e1, e2, e3, A_IK_basic, norm, cross3, Log_SO3_quat
+
+from cardillo.visualization import Export
+
+from cardillo import System
+
+from math import pi
+import numpy as np
+
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+'''
+Example proposed by Goto, Y. et al,
+"Elastic buckling phenomenon applicable to deployable rings" ,
+International Journal of Solids and Structures, 29(7):893– 909, 1992,
+https://sci-hub.hkvisa.net/10.1016/0020-7683(92)90024-n, 1988
+'''
+
+def deployment_of_elastic_ring(load_type="moment", rod_hypothesis_penalty="shear_deformable", VTK_export=False):
+    
+    # Rod = CosseratRodPG_R12Mixed
+    Rod = CosseratRodPG_QuatMixed
+    # Rod = CosseratRodPG_SE3Mixed
+
+    nelements_Lagrangian = 20
+    polynomial_degree = 1
+
+     # number of elements
+    if Rod is CosseratRodPG_SE3Mixed:
+        nelements = nelements_Lagrangian * polynomial_degree
+    else:
+        nelements = nelements_Lagrangian
+
+    # geometry of the rod
+    length = 2 * pi * 100 
+    width = 1/3.
+    height = 1
+
+    # cross section
+    line_density = 1
+    cross_section = RectangularCrossSection(line_density, width, height)
+    
+    A = cross_section.area
+    A_rho0 = line_density * cross_section.area
+    K_S_rho0 = line_density * cross_section.first_moment
+    K_I_rho0 = line_density * cross_section.second_moment
+
+    atol = 1e-10
+
+    EE = 2.1 * 1e7 # Young's modulus
+    GG = EE / (2 * (1 + 0.3)) # shear modulus
+   
+    # material model
+    if rod_hypothesis_penalty == "shear_deformable":
+        Ei = np.array([EE * A, GG * A,  GG * A])
+    elif rod_hypothesis_penalty == "shear_rigid":
+        Ei = np.array([EE * A, 1e10 * GG * A, 1e10 * GG * A])
+    elif rod_hypothesis_penalty == "inextensilbe_shear_rigid":
+        Ei = np.array([EE * A, GG * A,  GG * A]) * 1e10
+            
+    Fi = np.array([GG * K_I_rho0[0,0], EE * K_I_rho0[1,1], EE * K_I_rho0[2,2]])
+
+    material_model = Simo1986(Ei, Fi)
+
+    R = 20
+    angle = pi
+
+    # definition of the parametric curve
+    curve = lambda xi: np.array([R - R * np.cos(xi), R * np.sin(xi), 0])
+    dcurve = lambda xi: np.array([R * np.sin(xi), R * np.cos(xi), 0])
+    ddcurve = lambda xi: np.array([R * np.cos(xi), -R * np.sin(xi), 0])
+
+    # starting point and orientation of initial point, initial length
+    r_OP0 = np.zeros(3, dtype=float)
+    A_IK0 = np.eye(3, dtype=float)
+
+    q0 = Rod.deformed_configuration(
+        nelements,
+        curve,
+        dcurve,
+        ddcurve,
+        angle,
+        polynomial_degree=polynomial_degree,
+        r_OP=r_OP0,
+        A_IK=A_IK0,
+        mixed=True,
+    )
+
+    cantilever = Rod(
+        cross_section,
+        material_model,
+        A_rho0,
+        K_S_rho0,
+        K_I_rho0,
+        nelements,
+        Q=q0,
+        q0=q0,
+        polynomial_degree=polynomial_degree,
+        reduced_integration=False,
+        mixed=True
+    )
+
+    # create the system
+    system = System()
+
+    # generate the constraint on the beam
+    r_OP_2_end = np.zeros(3, dtype=float)
+    r_OP_2_end[0] = 2 * R
+    clamping_point = Frame(A_IK=A_IK0)
+
+    A_IK_displ = lambda t: A_IK_basic(t * 2 * pi).x()
+    displ_imposed = Frame(r_OP_2_end, A_IK_displ)
+    clamping_left = RigidConnection(clamping_point, cantilever, frame_ID2=(0,))
+    clamping_right = Cylindrical(displ_imposed, cantilever, 0, frame_ID2=(1,))
+    
+    
+    # assemble the system
+    system.add(cantilever)
+    system.add(displ_imposed)
+    system.add(clamping_point)
+    system.add(clamping_left)
+    system.add(clamping_right)
+    system.assemble()
+
+
+    solver = Newton(
+        system,
+        n_load_steps=60,
+        max_iter=30,
+        atol=atol,
+    )
+
+    sol = solver.solve()
+    q = sol.q
+    nt = len(q)
+    t = sol.t[:nt]
+
+    # matplotlib visualization
+    # construct animation of beam
+    fig1, ax1, anim1 = animate_beam(
+    t,
+    q, 
+    [cantilever],
+    scale=2*R,
+    scale_di=0.05,
+    show=False,
+    n_frames=cantilever.nelement + 1,
+    repeat=True,
+    )
+
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    deployment_of_elastic_ring(load_type="moment", VTK_export=False)
+ 
