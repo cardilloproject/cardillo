079ef1ac87a4b0dd28b2b9ca3b494f6c5eb6f5b1
Jonas Harsch <jonas.harsch@inm.uni-stuttgart.de>
2023-10-26 16:20:00 +0200

    Moved rolling condition into example.

--- cardillo/constraints/__init__.py
+++ cardillo/constraints/__init__.py
@@ -4,5 +4,4 @@ from .revolute import Revolute
 from .prismatic import Prismatic
 from .cylindrical import Cylindrical
 from .planarizer import Planarizer
-from .rolling_conditions import *
 from .fixed_distance import FixedDistance
