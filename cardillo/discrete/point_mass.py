fix sign in h_q

cardillo/discrete/point_mass.py
@@ -28,7 +28,7 @@ class PointMass:
     def q_ddot(self, t, q, u, u_dot):
         return u_dot
 
-    def B(self, t, q):
+    def q_dot_u(self, t, q, u):
         return np.ones(self.nq)
 
     #####################

cardillo/forces/maxwell_element.py
@@ -10,7 +10,7 @@ class MaxwellElement:
         subsystem2,
         stiffness,
         viscosity,
-        g_sref=None, # undeformed lenght of spring
+        g_sref=None,  # undeformed lenght of spring
         q0=np.zeros(1),
         frame_ID1=np.zeros(3, dtype=float),
         frame_ID2=np.zeros(3, dtype=float),
@@ -34,7 +34,7 @@ class MaxwellElement:
     def assembler_callback(self):
         qDOF1 = self.subsystem1.qDOF
         qDOF2 = self.subsystem2.qDOF
-        qDOF3 = self.qDOF
+        qDOF3 = self.qDOF.copy()
         local_qDOF1 = self.subsystem1.local_qDOF_P(self.frame_ID1)
         local_qDOF2 = self.subsystem2.local_qDOF_P(self.frame_ID2)
         self.qDOF = np.concatenate((qDOF1[local_qDOF1], qDOF2[local_qDOF2], qDOF3))
@@ -99,13 +99,21 @@ class MaxwellElement:
             t, q[self._nq1 : self._nq], self.frame_ID2, self.K_r_SP2
         )
         self.J_P2_q = lambda t, q: self.subsystem2.J_P_q(
-            t, q[self._nq1 :self._nq1 + self._nq2], self.frame_ID2, self.K_r_SP2
+            t, q[self._nq1 : self._nq1 + self._nq2], self.frame_ID2, self.K_r_SP2
         )
         self.v_P2 = lambda t, q, u: self.subsystem2.v_P(
-            t, q[self._nq1 :self._nq1 + self._nq2], u[self._nu1 :], self.frame_ID2, self.K_r_SP2
+            t,
+            q[self._nq1 : self._nq1 + self._nq2],
+            u[self._nu1 :],
+            self.frame_ID2,
+            self.K_r_SP2,
         )
         self.v_P2_q = lambda t, q, u: self.subsystem2.v_P_q(
-            t, q[self._nq1 :self._nq1 + self._nq2], u[self._nu1 :], self.frame_ID2, self.K_r_SP2
+            t,
+            q[self._nq1 : self._nq1 + self._nq2],
+            u[self._nu1 :],
+            self.frame_ID2,
+            self.K_r_SP2,
         )
 
     # auxiliary functions
@@ -190,10 +198,10 @@ class MaxwellElement:
 
     def q_dot(self, t, q, u):
         g_d = q[-1]
-        return  (self.stiffness/self.viscosity) * (self._g(t, q) - g_d - self.g_sref)
-    
+        return (self.stiffness / self.viscosity) * (self._g(t, q) - g_d - self.g_sref)
+
     def q_dot_q(self, t, q, u):
-        q_dot_q = (self.stiffness / self.viscosity) * self._g_dot_q(t, q, u)
+        q_dot_q = (self.stiffness / self.viscosity) * self._g_q(t, q)
         q_dot_q[-1] -= self.stiffness / self.viscosity
         return q_dot_q
 
@@ -206,16 +214,15 @@ class MaxwellElement:
 
     def h(self, t, q, u):
         g_d = q[-1]
-        return - self._W(t, q) * self.stiffness * (self._g(t, q) - g_d - self.g_sref)
+        return -self._W(t, q) * self.stiffness * (self._g(t, q) - g_d - self.g_sref)
 
     def h_q(self, t, q, u):
         g_d = q[-1]
-        h_q = (
-            self._W_q(t, q) * self.stiffness * (self._g(t, q) - g_d - self.g_s0)
+        h_q = -(
+            self._W_q(t, q) * self.stiffness * (self._g(t, q) - g_d - self.g_sref)
             + np.outer(self._W(t, q), self._g_q(t, q)) * self.stiffness
         )
         h_q[:, -1] -= self._W(t, q) * self.stiffness
-        # h_q = approx_fprime(q, lambda q: self.h(t, q, u))
         return h_q
 
     # E_pot and h_u defined in init if necessary

test/test_maxwell_element.py
@@ -148,13 +148,14 @@ class MaxwellElementCompliance:
 
 class MaxwellElementForceElement:
     def __init__(self, mass, stiffness, damping, l0, x0, g_d0, x_dot0):
-        
         self.system = System()
         pm = PointMass(mass, q0=np.array([x0, 0, 0]), u0=np.array([x_dot0, 0, 0]))
         pm.name = "point mass"
         self.system.add(pm)
-        max = MaxwellElementFL(self.system.origin, pm, stiffness, damping, g_sref=l0, q0=np.array([g_d0]))
-        max.name = 'Maxwell-element'
+        max = MaxwellElementFL(
+            self.system.origin, pm, stiffness, damping, g_sref=l0, q0=np.array([g_d0])
+        )
+        max.name = "Maxwell-element"
         self.system.add(max)
         self.system.assemble()
 
@@ -168,32 +169,34 @@ if __name__ == "__main__":
     damping = 1
     l0 = 1
     x0 = 1.5
-    x_D0 = 0.
-    x_dot0 = 0.
+    x_D0 = 0.0
+    x_dot0 = 0.0
     q0 = np.array([x0, x_D0], dtype=float)
     u0 = np.array([x_dot0], dtype=float)
 
     # maxwell_element = MaxwellElement(mass, stiffness, damping, l0, q0, u0)
     # maxwell_element = MaxwellElementCompliance(mass, stiffness, damping, l0, q0, u0)
-   
+
     # system = System()
     # system.add(maxwell_element)
     # system.assemble()
 
-    system = MaxwellElementForceElement(mass, stiffness, damping, l0, x0, x_D0, x_dot0).get_system()
-
+    system = MaxwellElementForceElement(
+        mass, stiffness, damping, l0, x0, x_D0, x_dot0
+    ).get_system()
 
     t0 = 0
-    t1 = 0.2
+    t1 = 2
     dt = 5e-3
-    sol = EulerBackward(system, t1, dt).solve()
+    # sol = EulerBackward(system, t1, dt, debug=False).solve()
+    sol = ScipyIVP(system, t1, dt).solve()
 
     # - ref. solution
     def eqm(t, z):
         x, x_d, u = z
         dx = u
-        dx_d = (stiffness/damping) * (x - x_d - l0)
-        du = -(stiffness/mass) * (x - x_d - l0)
+        dx_d = (stiffness / damping) * (x - x_d - l0)
+        du = -(stiffness / mass) * (x - x_d - l0)
         return np.array([dx, dx_d, du])
 
     sol_ref = solve_ivp(eqm, [0, t1], np.array([x0, x_D0, x_dot0]))
@@ -202,16 +205,15 @@ if __name__ == "__main__":
 
     t, q, u = sol.t, sol.q, sol.u
 
-
     fig, ax = plt.subplots(1, 2)
     ax[0].plot(t, q[:, 0], "-b", label="x")
-    ax[0].plot(t_ref, z_ref[0], "--b", label='x_ref')
+    ax[0].plot(t_ref, z_ref[0], "--b", label="x_ref")
     ax[0].plot(t, q[:, -1], "-r", label="x_D")
-    ax[0].plot(t_ref, z_ref[1], "--r", label='x_D_ref')
+    ax[0].plot(t_ref, z_ref[1], "--r", label="x_D_ref")
     ax[0].grid()
     ax[0].legend()
     ax[1].plot(t, u[:, 0], label="x_dot")
-    ax[1].plot(t_ref, z_ref[2], "-b", label='x_dot_ref')
+    ax[1].plot(t_ref, z_ref[2], "-b", label="x_dot_ref")
     ax[1].grid()
 
     ax[0].legend()