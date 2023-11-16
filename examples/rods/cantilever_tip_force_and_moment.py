932634f1d7c9f46a38c8fde778b6d6bd9566fde2
SimonREugster <30239455+eugstesi@users.noreply.github.com>
2023-11-16 22:51:47 +0100

    reduced to just one mesh for la_c in rods

--- examples/rods/cantilever_tip_force_and_moment.py
+++ examples/rods/cantilever_tip_force_and_moment.py
@@ -235,11 +235,11 @@ if __name__ == "__main__":
 
     # For shear-rigid rods Harsch2021 and Simo1986 coincide. For Simo1986 also more robust mixed formulations are available
     cantilever(
-        Rod=make_CosseratRod_R12(mixed=False, constraints=[0, 1, 2]),
+        Rod=make_CosseratRod_R12(mixed=True),
         nelements=10,
         polynomial_degree=2,
         n_load_steps=3,
-        mixed=False,
+        rod_hypothesis_penalty="shear_rigid",
         reduced_integration=True,
         constitutive_law=Simo1986,
     )
