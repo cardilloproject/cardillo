d26b061479bfb9099dbd36c210830881acdb87d3
Giuseppe Capobianco <capobiag@gmail.com>
2023-12-01 12:07:22 +0100

    small changes to parameters

--- test/test_meshed_rigid_bodies.py
+++ test/test_meshed_rigid_bodies.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from cardillo import System
 from cardillo.forces import Force
 from cardillo.discrete import RigidBody, Frame, Meshed
-from cardillo.math import A_IK_basic
+from cardillo.math import A_IK_basic, Spurrier
 from cardillo.solver import BackwardEuler
 
 from cardillo.visualization import Export
@@ -15,27 +15,27 @@ if __name__=="__main__":
 
     cube_mesh = trimesh.creation.box(extents=[1,1,1])
     plane_mesh = cube_mesh.copy().apply_transform(np.diag([1,1,0.01,1]))
-    box_mesh = trimesh.creation.box(extents=[1,1,1])
+    box_mesh = trimesh.creation.box(extents=[0.2,0.2,0.1])
 
     MeshedFrame = Meshed(Frame)
     MeshedRB = Meshed(RigidBody)
 
     frame = MeshedFrame(plane_mesh, K_r_SP=np.array([0, 0, 0]), A_KM=A_IK_basic(np.pi/10).x())
     
-    q0 = np.array([0,0,1,1,0,0,0])
-    rigid_body = MeshedRB(box_mesh, density=1, mass=1, K_Theta_S=np.eye(3), q0=q0)#K_r_SP=np.array([0, 0, 1]), A_KM=A_IK_basic(np.pi/10).x())
+    q0 = np.concatenate([np.array([0,0,1]), Spurrier(A_IK_basic(np.pi/4).x())])
+    rigid_body = MeshedRB(box_mesh, density=2, mass=1, K_Theta_S=np.eye(3), q0=q0)#K_r_SP=np.array([0, 0, 1]), A_KM=A_IK_basic(np.pi/10).x())
 
     scene = trimesh.Scene()
     scene.add_geometry(trimesh.creation.axis(origin_size=0.05))
     scene.add_geometry(frame.get_visual_mesh_wrt_I(0, 0))
     scene.add_geometry(rigid_body.get_visual_mesh_wrt_I(0, q0))
     # scene.add_geometry(trimesh.creation.axis(origin_size=0.1, transform=box.visual_mesh.bounding_box_oriented.transform))
-    scene.show()
+    
 
     system = System()
     system.add(frame)
     system.add(rigid_body)
-    system.add(Force(np.array([0,0,-1]), rigid_body))
+    system.add(Force(np.array([0,0,-1 * rigid_body.mass]), rigid_body))
     system.assemble()
 
     sol = BackwardEuler(system, 1, 1e-1).solve()
@@ -51,3 +51,5 @@ if __name__=="__main__":
     e.export_contr(system.origin)
     e.export_contr(frame)
     e.export_contr(rigid_body)
+
+    scene.show() # this ends the execution of the file!!!!
