e3fb59bd444624a236f1d2011b9e7d7ab2141cab
tianxdai <tianxdai@gmail.com>
2024-05-02 12:42:48 +0200

    add arguments for rigidconnection

--- cardillo/constraints/rigid_connection.py
+++ cardillo/constraints/rigid_connection.py
@@ -10,15 +10,17 @@ class RigidConnection(PositionOrientationBase):
         self,
         subsystem1,
         subsystem2,
+        r_OJ0=None,
+        A_IJ0=None,
         xi1=None,
         xi2=None,
     ):
         super().__init__(
             subsystem1,
             subsystem2,
-            r_OJ0=None,
-            A_IJ0=None,
-            projection_pairs_rotation=[(0, 1), (1, 2), (2, 0)],
+            r_OJ0=r_OJ0,
+            A_IJ0=A_IJ0,
+            projection_pairs_rotation=[(1, 2), (2, 0), (0, 1)],
             xi1=xi1,
             xi2=xi2,
         )
