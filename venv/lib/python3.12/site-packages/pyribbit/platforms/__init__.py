14a185bee1796a2e984e4c3714bcc788dcc3b676
RISHYAVANDHAN <rishyavandhan.v@gmail.com>
2024-12-26 21:53:33 +0100

    Delete venv directory

--- venv/lib/python3.12/site-packages/pyribbit/platforms/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Platforms for generating offscreen OpenGL contexts for rendering.
-
-Author: Matthew Matl
-"""
-
-from .base import Platform
--- venv/lib/python3.12/site-packages/trimesh/interfaces/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import blender, gmsh
-
-# add to __all__ as per pep8
-__all__ = ["blender", "gmsh"]
