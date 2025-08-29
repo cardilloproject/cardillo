c8aedf546d9ff397a3316fa66bb739dfc9e29d96
Giuseppe Capobianco <capobiag@gmail.com>
2025-08-29 14:27:14 +0200

    cleaned up the examples

--- examples/urdf_double_pendulum/double_pendulum.py
+++ examples/urdf_double_pendulum/double_pendulum.py
@@ -1,33 +1,35 @@
 import numpy as np
 from cardillo.urdf import system_from_urdf
-from cardillo.discrete import Sphere, Frame
 from cardillo.math import A_IB_basic
 from cardillo.visualization import Renderer
 from cardillo.solver import Solution, ScipyIVP
 
+# Initial joint configuration and velocities
 configuration = {
     "joint1": np.pi,
     "joint2": np.pi / 2,
 }
-velocity = {
+velocities = {
     "joint1": 1.0,
-    # "joint2": 5.0,
 }
 
+# Build the system from URDF
 system = system_from_urdf(
     "examples/urdf_double_pendulum/urdf/double_pendulum.urdf",
     r_OR=np.array([0.05, 0, 0.1]),
     A_IR=A_IB_basic(np.pi / 4).x,
     configuration=configuration,
-    velocities=velocity,
+    velocities=velocities,
     root_is_floating=False,
     gravitational_acceleration=np.array([0, 0, -9.81]),
 )
 
+# Visualization
 render = Renderer(system)
 
-if simulate := True:
-    sol = ScipyIVP(system, t1=3, dt=1e-2).solve()
+simulate = True  # Set to False to show initial configuration only
+if simulate:
+    sol = ScipyIVP(system, t1=2.0, dt=1e-2).solve()
     render.render_solution(sol, repeat=True)
 else:
     render.render_solution(
--- examples/urdf_unitree_go1/go1.py
+++ examples/urdf_unitree_go1/go1.py
@@ -1,70 +1,59 @@
+
 import numpy as np
-
-
-from cardillo import System
-from cardillo.actuators import PDcontroller
-from cardillo.urdf import system_from_urdf
-from cardillo.math import A_IB_basic
 from pathlib import Path
-from cardillo.solver import Moreau, Solution, SolverOptions
-from cardillo.discrete import Box, Frame
+from cardillo.urdf import system_from_urdf
+from cardillo.actuators import PDcontroller
 from cardillo.contacts import Sphere2Plane
+from cardillo.discrete import Box, Frame
 from cardillo.visualization import Renderer
+from cardillo.solver import Moreau, Solution
 
 if __name__ == "__main__":
     from os import path
 
+    # Get directory of this script
     dir_name = path.dirname(__file__)
 
-    initial_config = {}
+    # Initial joint configuration for Unitree Go1 robot
+    initial_config = {
+        "FR_hip_joint": -np.pi / 12,
+        "FR_thigh_joint": np.pi / 4,
+        "FR_calf_joint": -np.pi / 2,
+        "FL_hip_joint": np.pi / 12,
+        "FL_thigh_joint": np.pi / 4,
+        "FL_calf_joint": -np.pi / 2,
+        "RL_hip_joint": np.pi / 12,
+        "RL_thigh_joint": np.pi / 4,
+        "RL_calf_joint": -np.pi / 2,
+        "RR_hip_joint": -np.pi / 12,
+        "RR_thigh_joint": np.pi / 4,
+        "RR_calf_joint": -np.pi / 2,
+        "world_trunk": np.array([0, 0, 0.4, 0, 0, 0]),
+    }
 
-    initial_config["FR_hip_joint"] = -np.pi / 12  # np.pi / 2
-    initial_config["FR_thigh_joint"] = np.pi / 4
-    initial_config["FR_calf_joint"] = -np.pi / 2
+    # List of joint names (excluding world_trunk)
+    joint_names = [k for k in initial_config.keys() if k != "world_trunk"]
 
-    initial_config["FL_hip_joint"] = np.pi / 12  # np.pi / 2
-    initial_config["FL_thigh_joint"] = np.pi / 4
-    initial_config["FL_calf_joint"] = -np.pi / 2
-
-    initial_config["RL_hip_joint"] = np.pi / 12  # np.pi / 2
-    initial_config["RL_thigh_joint"] = np.pi / 4
-    initial_config["RL_calf_joint"] = -np.pi / 2
-
-    initial_config["RR_hip_joint"] = -np.pi / 12  # np.pi / 2
-    initial_config["RR_thigh_joint"] = np.pi / 4
-    initial_config["RR_calf_joint"] = -np.pi / 2
-
-    initial_config["world_trunk"] = np.array([0, 0, 0.4, 0, 0, 0])
-
-    joint_names = list(initial_config.keys())
-    joint_names.pop(-1)
-
+    # Build system from URDF
     system = system_from_urdf(
         file_path=Path(dir_name, "urdf", "go1.urdf"),
         configuration=initial_config,
         gravitational_acceleration=np.array([0, 0, -9.81]),
     )
+
+    # Add foot contact models
     radius = 0.022
     mu = 0.3
-    foot_contact_FR = Sphere2Plane(
-        system.origin, system.contributions_map["FR_foot"], mu=mu, r=radius
-    )
-    foot_contact_FL = Sphere2Plane(
-        system.origin, system.contributions_map["FL_foot"], mu=mu, r=radius
-    )
-    foot_contact_RR = Sphere2Plane(
-        system.origin, system.contributions_map["RR_foot"], mu=mu, r=radius
-    )
-    foot_contact_RL = Sphere2Plane(
-        system.origin, system.contributions_map["RL_foot"], mu=mu, r=radius
-    )
-    system.add(foot_contact_FR, foot_contact_FL, foot_contact_RR, foot_contact_RL)
+    for foot in ["FR_foot", "FL_foot", "RR_foot", "RL_foot"]:
+        contact = Sphere2Plane(system.origin, system.contributions_map[foot], mu=mu, r=radius)
+        system.add(contact)
 
-    if PD_joint_controller := True:
+    # Add PD controllers to each joint
+    PD_joint_controller = True
+    if PD_joint_controller:
         kp = 50
         kd = 1
         for joint_name in joint_names:
-            # controller = PDcontroller(system.contributions_map[joint_name], kp, kd, np.array([0, 0]))
             motor = PDcontroller(
                 system.contributions_map[joint_name],
                 kp,
@@ -74,38 +63,24 @@ if __name__ == "__main__":
             motor.name = "PD_" + system.contributions_map[joint_name].name
             system.add(motor)
 
-        # kp_hip = 30
-        # system.contributions_map["PD_FR_hip_joint"].kp = kp_hip
-        # system.contributions_map["PD_FL_hip_joint"].kp = kp_hip
-        # system.contributions_map["PD_RR_hip_joint"].kp = kp_hip
-        # system.contributions_map["PD_RL_hip_joint"].kp = kp_hip
+        # Example: Add time-varying torque to calf joints (currently static)
         frq = 0
         angle_d_calf = -np.deg2rad(30)
-        for joint_name in [
-            "FR_calf_joint",
-            "FL_calf_joint",
-            "RR_calf_joint",
-            "RL_calf_joint",
-        ]:
-            system.contributions_map["PD_" + joint_name].tau = lambda t: np.array(
-                [
-                    initial_config[joint_name]
-                    + angle_d_calf * np.sin(2 * np.pi * frq * t),
-                    angle_d_calf * 2 * np.pi * frq * np.cos(2 * np.pi * frq * t),
-                ]
-            )
+        for joint_name in ["FR_calf_joint", "FL_calf_joint", "RR_calf_joint", "RL_calf_joint"]:
+            system.contributions_map["PD_" + joint_name].tau = lambda t: np.array([
+                initial_config[joint_name] + angle_d_calf * np.sin(2 * np.pi * frq * t),
+                angle_d_calf * 2 * np.pi * frq * np.cos(2 * np.pi * frq * t),
+            ])
 
-            # angle_d_thigh = np.deg2rad(2)
-            # for joint_name in ["FR_thigh_joint", "FL_thigh_joint", "RR_thigh_joint", "RL_thigh_joint"]:
-            #     system.contributions_map["PD_" + joint_name].tau = lambda t: np.array([initial_config[joint_name] + angle_d_thigh * np.sin(2 * np.pi * frq * t), angle_d_thigh * 2 * np.pi * frq * np.cos(2 * np.pi * frq * t)])
-
+    # Add ground plane
     plane = Box(Frame)(dimensions=[4, 4, 0.001], axis=2, name="Plane")
-
     system.add(plane)
     system.assemble()
 
+    # Visualization and simulation
     render = Renderer(system)
-    if simulate := True:
+    simulate = True  # Set to False to show initial configuration only
+    if simulate:
         sol = Moreau(system, 0.25, 1e-3).solve()
         render.render_solution(sol, repeat=True)
     else:
